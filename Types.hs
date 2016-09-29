476c6c5c0d1cec9315bef1b8af97d7e3837477f1
Ben Gamari <ben@smart-cactus.org>
2016-09-29 00:08:59 -0400

    Add helper to read encoded values

--- Types.hs
+++ Types.hs
@@ -4,6 +4,7 @@ module Types where
 
 import GHC.Generics
 import qualified Data.Binary.Serialise.CBOR as CBOR
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import Data.MediaWiki.Markup
 
@@ -22,3 +23,12 @@ instance CBOR.Serialise ParaBody
 data Page = Page { pageName :: PageName, pageSkeleton :: [PageSkeleton] }
           deriving (Show, Generic)
 instance CBOR.Serialise Page
+
+readValues :: CBOR.Serialise a => BSL.ByteString -> [a]
+readValues = start . BSL.toChunks
+  where
+    start = go CBOR.deserialiseIncremental
+    go _ [] = error "ran out of data"
+    go (CBOR.Partial f) (bs : bss) = go (f (Just bs)) bss
+    go (CBOR.Done bs _ x) bss = x : start (bs : bss)
+    go (CBOR.Fail rest _ err) _ = error $ show err
