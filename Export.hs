4baaecbb9a71f3ce70daa04d2aacca860e6adc54
Ben Gamari <ben@smart-cactus.org>
2016-11-14 15:41:39 -0500

    car-export: More exports

--- Export.hs
+++ Export.hs
@@ -6,7 +6,7 @@ import Options.Applicative
 
 import CAR.AnnotationsFile
 import CAR.Types
-import CAR.CarExports
+import CAR.CarExports as Exports
 
 options :: Parser (FilePath)
 options =
@@ -17,10 +17,22 @@ main = do
     (path) <- execParser $ info (helper <*> options) mempty
     anns <- openAnnotations path
 
-    putStrLn "Writing skeletons..."
+    putStrLn "Writing stubs skeletons..."
     let skeletonFile = path <.> "skeletons"
     withFile skeletonFile WriteMode $ \h ->
         BSB.hPutBuilder h $ encodeCborList $ map toStubSkeleton (pages anns)
+    putStrLn "done"
+
+    putStrLn "Writing paragraphs..."
+    let paragraphsFile = path <.> "paragraphs"
+    withFile paragraphsFile WriteMode $ \h ->
+        BSB.hPutBuilder h $ encodeCborList $ map toParagraphs (pages anns)
+    putStrLn "done"
+
+    putStrLn "Writing relevance annotations..."
+    let relsFile = path <.> "qrel"
+    withFile relsFile WriteMode $ \h ->
+          hPutStrLn h $ unlines $ map prettyAnnotation $ concatMap Exports.toAnnotations $ (pages anns)
     putStrLn "done"
 
     return ()
