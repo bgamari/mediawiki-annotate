49a8ae165f8919edfa359a82e6ff15fa4ed19e3a
Ben Gamari <ben@smart-cactus.org>
2016-11-17 15:23:02 -0500

    Hack export

--- Export.hs
+++ Export.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Maybe
 import qualified Data.ByteString.Builder as BSB
 import System.IO
 import System.FilePath
@@ -16,23 +19,32 @@ main :: IO ()
 main = do
     (path) <- execParser $ info (helper <*> options) mempty
     anns <- openAnnotations path
+    let names = [ "Green sea turtle"
+                , "Hydraulic fracturing"
+                , "Spent nuclear fuel"
+                , "Food vs. fuel"
+                , "Sustainable biofuel"
+                , "Behavior of nuclear fueld during a reactor accident"
+                ]
+    let pages = mapMaybe (`lookupPage` anns) names
+        {-# INLINE pages #-}
 
     putStr "Writing stub skeletons..."
     let skeletonFile = path <.> "skeletons"
     withFile skeletonFile WriteMode $ \h ->
-        BSB.hPutBuilder h $ encodeCborList $ map toStubSkeleton (pages anns)
+        BSB.hPutBuilder h $ encodeCborList $ map toStubSkeleton pages
     putStrLn "done"
 
     putStr "Writing paragraphs..."
     let paragraphsFile = path <.> "paragraphs"
     withFile paragraphsFile WriteMode $ \h ->
-        BSB.hPutBuilder h $ encodeCborList $ concatMap toParagraphs (pages anns)
+        BSB.hPutBuilder h $ encodeCborList $ concatMap toParagraphs pages
     putStrLn "done"
 
     putStr "Writing relevance annotations..."
     let relsFile = path <.> "qrel"
     withFile relsFile WriteMode $ \h ->
-          hPutStr h $ unlines $ map prettyAnnotation $ concatMap Exports.toAnnotations $ (pages anns)
+          hPutStr h $ unlines $ map prettyAnnotation $ concatMap Exports.toAnnotations pages
     putStrLn "done"
 
     return ()
