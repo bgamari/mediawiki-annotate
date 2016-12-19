24f29de1adc556c5e1a33fd1226272ae68281b98
Ben Gamari <ben@smart-cactus.org>
2016-12-19 10:32:15 -0500

    Import: Exclude File talk

--- Import.hs
+++ Import.hs
@@ -59,6 +59,7 @@ isInteresting :: WikiDoc -> Bool
 isInteresting WikiDoc{..} = not $
        "#REDIRECT" `BS.isInfixOf` BS.take 20 docText
     || "Talk:" `BS.isPrefixOf` docTitle
+    || "File talk:" `BS.isPrefixOf` docTitle
     || "Special:" `BS.isPrefixOf` docTitle
     || "User:" `BS.isPrefixOf` docTitle
     || "User talk:" `BS.isPrefixOf` docTitle
