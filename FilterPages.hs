42c28b3a55a39ef12fe1bbbd5817b8c49dcacfd9
Ben Gamari <ben@smart-cactus.org>
2017-01-08 17:14:17 -0500

    FilterPages: Add is-redirect predicate

--- FilterPages.hs
+++ FilterPages.hs
@@ -20,6 +20,7 @@ opts =
     predicate :: Parser (IO (Page -> Bool))
     predicate =
         testPred <|> trainPred <|> namePred <|> categoryPred
+        <|> redirectPred
 
     testPred = flag' f (long "test")
       where f = pure $ even . hash . pageName
@@ -41,22 +42,39 @@ opts =
                 let matches cat = any (\p ->  p `T.isInfixOf` cat) patterns
                 return $ \page -> any (\cat -> matches $ T.toCaseFold cat) (pageCategories page)
 
+    redirectPred = flag' (pure pageIsRedirect) (long "redirect")
+
+pageIsRedirect :: Page -> Bool
+pageIsRedirect (Page {pageSkeleton=[Para (Paragraph _ (ParaText t:_))]}) =
+    T.pack "#redirect" `T.isPrefixOf` T.toCaseFold (T.stripStart t)
+pageIsRedirect _ = False
+
+pageContainsText :: T.Text -> Page -> Bool
+pageContainsText str = any goSkeleton . pageSkeleton
+  where
+    goSkeleton (Section _ _ children) = any goSkeleton children
+    goSkeleton (Para (Paragraph _ bodies)) = any goParaBody bodies
+
+    goParaBody (ParaLink _ t) = str `T.isInfixOf` t
+    goParaBody (ParaText t)   = str `T.isInfixOf` t
+
 pageCategories :: Page -> [T.Text]
-pageCategories page =
-    mapMaybe isCategoryTag $ foldMap skeletonLinks $ pageSkeleton page
+pageCategories = mapMaybe isCategoryTag . pageLinkTargets
+  where
+    isCategoryTag :: PageName -> Maybe T.Text
+    isCategoryTag (PageName pageName) =
+        T.pack "Category:" `T.stripPrefix` pageName
+
+pageLinkTargets :: Page -> [PageName]
+pageLinkTargets = foldMap skeletonLinks . pageSkeleton
   where
     skeletonLinks :: PageSkeleton -> [PageName]
     skeletonLinks (Section _ _ children) = foldMap skeletonLinks children
     skeletonLinks (Para (Paragraph _ bodies)) = foldMap paraBodyLinks bodies
 
     paraBodyLinks :: ParaBody -> [PageName]
-    paraBodyLinks (ParaLink pageName _ ) = [pageName]
+    paraBodyLinks (ParaLink pageName _) = [pageName]
     paraBodyLinks (ParaText _ ) = []
-
-    isCategoryTag :: PageName -> Maybe T.Text
-    isCategoryTag (PageName pageName) =
-        T.pack "Category:" `T.stripPrefix` pageName
-
 
 main :: IO ()
 main = do
