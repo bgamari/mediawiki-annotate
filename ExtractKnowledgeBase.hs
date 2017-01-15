4fc56551e43a6a9f214e1a76d070e4ff2074ec09
Laura Dietz <dietz@cs.umass.edu>
2017-01-15 17:34:47 -0500

    Export KB in WARC format, to be indexed with Galago and the EQFE setup.

--- ExtractKnowledgeBase.hs
+++ ExtractKnowledgeBase.hs
@@ -13,6 +13,8 @@ import qualified Data.HashSet as HS
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import qualified Data.Text.Lazy.Builder as TB
 import qualified Data.ByteString.Lazy as BSL
 import Data.Maybe
 
@@ -32,10 +34,40 @@ isLead (Section _ _ _) = False
 
 data KbDoc = KbDoc { kbDocPageId :: PageId
                    , kbDocCanonicalName :: PageName
-                   , kbDocLeadText :: [T.Text]
-                   , kbDocCategories :: [T.Text]
-                   , kbDocOutLinks :: [PageName]
+                   , kbDocLeadText :: [T.Text]    -- ^ Page's lead paragraph text
+                   , kbDocCategories :: [T.Text]  -- ^ Page's categories
+                   , kbDocOutLinks :: [PageName]  -- ^ Targets of page's outgoing links
+                   , kbDocOutMentions :: [T.Text] -- ^ Anchor text of page's outgoing links
+                   , kbDocLeadPara :: [PageSkeleton]   -- ^ 'PageSkeleton's of lead paragraph text
                    }
+
+wikiAnchorStopwords = [ "more....", "wikipedia article", "source: wikipedia", "here", "wiki", "wikipedia"]
+wikiAnchorStopphrases [ "wikipedia the free encyclopedia", "en.wikipedia.org", "full article at wikipedia.org",
+                        "wikipedia the free encyclopedia", "wikipedia, the free encyclopedia",
+                        "http://en.wikipedia.org/wiki/", "wikipedia"]
+
+
+skeletonToXml :: [PageSkeleton] -> TB.Builder
+skeletonToXml list =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<articles loadtime=\"0 sec\" rendertime=\"0.06 sec\" totaltime=\"0.06 sec\"><article> "
+    <> foldMap skelToXml list
+    <>" </article></articles>"
+  where
+
+    skelToXml :: PageSkeleton -> TB.Builder
+    skelToXml (Para (Paragraph paraId parabodies ) )  =
+        " <paragraph><sentence> "  -- id = 1
+        <> foldMap bodyToXml parabodies
+        <> " </sentence></paragraph> "
+    skelToXml (Section (SectionHeading heading) _ parabodies )  =
+        " <heading level=\"1\"> "<> TB.fromText (T.replace "\n" " " heading) <>" </heading> "  -- id = 1
+
+
+    bodyToXml :: ParaBody -> TB.Builder
+    bodyToXml (ParaText text) = " " <> TB.fromText (T.replace "\n" " " text) <> " "
+    bodyToXml (ParaLink (PageName pagename) anchor) =
+        " <link><target> "<>TB.fromText pagename<>" </target></link> "
+
 
 data InlinkInfo = InlinkInfo { documentInlinks :: !(HM.HashMap PageName InlinkCounts)
                              , redirectPages   :: !(HS.HashSet PageName)
@@ -47,17 +79,19 @@ instance Monoid InlinkInfo where
         InlinkInfo (HM.unionWith mappend a a') (b<>b')
 
 data InlinkCounts = InlinkCounts { -- | How many time each anchor is used to link to this document.
-                                   anchorCount ::  !(HM.HashMap T.Text Int)
+                                   inLinkCounts        :: !(HM.HashMap PageName Int)
+                                 , anchorCount         :: !(HM.HashMap T.Text Int)
                                  , disambiguationCount :: !(HM.HashMap PageName Int)
-                                 , redirectCount :: !(HM.HashMap PageName Int)
+                                 , redirectCount       :: !(HM.HashMap PageName Int)
                                  }
 
 instance Monoid InlinkCounts where
-    mempty = InlinkCounts mempty mempty mempty
-    InlinkCounts a b c `mappend` InlinkCounts a' b' c' =
+    mempty = InlinkCounts mempty mempty mempty mempty
+    InlinkCounts a b c d `mappend` InlinkCounts a' b' c' d' =
         InlinkCounts (HM.unionWith (+) a a')
                      (HM.unionWith (+) b b')
                      (HM.unionWith (+) c c')
+                     (HM.unionWith (+) d d')
 
 ---  sourcepage targetpage anchortext => attach anchortext to targetpage
 ---  redirect sourcepage targetpage   => attach sourcepage to targetpage
@@ -99,7 +133,7 @@ collectInlinkInfo = foldMap pageInlinkInfo
       | otherwise  =
             let toInlinkInfo (linkTarget, anchorText) =
                    mempty { documentInlinks = HM.singleton linkTarget
-                                              $ mempty { anchorCount = one $ anchorText }
+                                              $ mempty { anchorCount = one $ anchorText, inLinkCounts = one $ linkTarget }
                           }
             in foldMap toInlinkInfo (pageLinks page)
 
@@ -110,6 +144,8 @@ transformContent inlinkInfoMap (Page pageName pageId pageSkeleta) =
       kbDocPageId = pageId
       kbDocLeadText = foldMap pageSkeletonText $ leadParas
       kbDocOutLinks = fmap fst $ foldMap pageSkeletonLinks $ leadParas
+      kbDocOutMentions = fmap snd $ foldMap pageSkeletonLinks $ leadParas
+      kbDocLeadPara = leadParas
       kbDocCategories = pageCategories (Page pageName pageId pageSkeleta)
       kbDocCanonicalName = pageName
 
@@ -122,23 +158,46 @@ transformContent inlinkInfoMap (Page pageName pageId pageSkeleta) =
 
 toGalagoDoc :: HM.HashMap PageName InlinkCounts -> InlinkCounts -> KbDoc -> Galago.Document
 toGalagoDoc inlinkInfoMap linkStats kbDoc =
-    let meta = M.fromList []
+    let inlinkInfo = fromMaybe mempty $ HM.lookup (kbDocCanonicalName kbDoc) inlinkInfoMap
+        xml = skeletonToXml (kbDocLeadPara kbDoc)
+        numAnchors = sum $ anchorCount inlinkInfo
+        numOutlinks = length $ kbDocOutLinks $ kbDoc
+        meta = M.fromList [
+                            ("fbname", TL.fromStrict $ getPageName $ kbDocCanonicalName kbDoc)
+                          , ("contextLinks", "") -- pagename 1\npage2name 2\n"
+                          , ("xml", TB.toLazyText xml)
+                          , ("kbId","")
+                          , ("lastModified", "2012-01-20 08:14:38")
+                          , ("inlink", TL.pack $ show numAnchors)
+                          , ("externalLinkCount", TL.pack $ show numOutlinks)
+                          , ("category",  TL.intercalate "," $ map TL.fromStrict $ kbDocCategories kbDoc) -- "Category:Healthcare occupations,Category:Occupational therapy")
+                          , ("kbName","")
+                          , ("title", TL.fromStrict $ getPageName $ kbDocCanonicalName kbDoc)
+                          , ("fbtype", "") -- /people/profession,/base/tagit/concept
+                          , ("kbType", "")
+                          , ("srcInlinks", TL.intercalate " " $ map (TL.fromStrict . getPageName) $ HM.keys $ inLinkCounts inlinkInfo) --"Medical_credentials United_States_Public_Health_Service_Commissioned_Corps University_College_of_Northern_Denmark Home_care"
+                          ]
         phrase x = (TL.fromStrict x, Galago.DoNotTokenize)
         naturalText x = (TL.fromStrict x, Galago.DoTokenize)
-        inlinkInfo = fromMaybe mempty $ HM.lookup (kbDocCanonicalName kbDoc) inlinkInfoMap
         repeated :: HM.HashMap a Int -> [a]
         repeated = foldMap f . HM.toList
           where f (x,n) = replicate n x
     in Galago.Document { docId       = Galago.DocumentId $ T.pack $ unpackPageId $ kbDocPageId kbDoc
-                       , docMetadata = meta
+                       , docMetadata = fmap TL.encodeUtf8 meta
                        , docFields   = M.fromList
-                          [ ("title",               [phrase $ getPageName $ kbDocCanonicalName kbDoc])
-                          , ("links",               map (phrase . getPageName) $ kbDocOutLinks kbDoc)
-                          , ("categories",          map phrase $ kbDocCategories kbDoc)
-                          , ("lead",                [naturalText $ T.unlines $ kbDocLeadText kbDoc])
-                          , ("anchornames",         map phrase $ repeated $ anchorCount $ inlinkInfo)
-                          , ("disambiguationnames", map (phrase . getPageName) $ repeated $ disambiguationCount $ inlinkInfo)
-                          , ("redirectnames",       map (phrase . getPageName) $ repeated $ redirectCount $ inlinkInfo )
+                          [ ("title",                [naturalText $ getPageName $ kbDocCanonicalName kbDoc])
+                          , ("title-exact",          [phrase $ getPageName $ kbDocCanonicalName kbDoc])
+                          , ("category",             map phrase $ kbDocCategories kbDoc)
+                          , ("lead",                 [naturalText $ T.unlines $ kbDocLeadText kbDoc])
+                          , ("anchor",               map naturalText $ repeated $ anchorCount $ inlinkInfo)
+                          , ("anchor-exact",         map phrase $ repeated $ anchorCount $ inlinkInfo)
+                          , ("disambiguation",       map (naturalText . getPageName) $ repeated $ disambiguationCount $ inlinkInfo)
+                          , ("disambiguation-exact", map (phrase . getPageName) $ repeated $ disambiguationCount $ inlinkInfo)
+                          , ("redirect",             map (naturalText . getPageName) $ repeated $ redirectCount $ inlinkInfo )
+                          , ("redirect-exact",       map (phrase . getPageName) $ repeated $ redirectCount $ inlinkInfo )
+                          , ("mentions",             map phrase $ kbDocOutMentions kbDoc) -- TODO
+                          , ("kblinks",              map (phrase . getPageName) $ kbDocOutLinks kbDoc)
+                          , ("wikititle",            map (phrase . T.replace "%20" "_" . getPageName) $ kbDocOutLinks kbDoc)
                           ]
                        }
 
