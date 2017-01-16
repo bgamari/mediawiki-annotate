4d9e88dbf4a46683bb07482d18540ab6c0abd12a
Laura Dietz <dietz@cs.umass.edu>
2017-01-15 22:46:31 -0500

    Bump mediawiki-parser submodule
    Moves PageName instances.

--- src/CAR/Types.hs
+++ src/CAR/Types.hs
@@ -50,14 +50,12 @@ urlEncodeText = SBS.pack . map (fromIntegral . ord) . escapeURIString isAllowedI
 
 -- Orphans
 deriving instance CBOR.Serialise PageName
-deriving instance IsString PageName
 instance FromJSON PageName
 instance FromJSONKey PageName where
     fromJSONKey = fmap PageName fromJSONKey
 instance ToJSON PageName
 instance ToJSONKey PageName where
     toJSONKey = contramapToJSONKeyFunction (\(PageName n) -> n) toJSONKey
-instance Hashable PageName
 
 instance CBOR.Serialise SBS.ShortByteString where
     encode = CBOR.encode . SBS.fromShort
