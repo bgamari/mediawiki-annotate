35af9bcf728de21e37e5b4d88c7a0dc41d5f764a
Laura Dietz <dietz@cs.umass.edu>
2017-01-17 23:53:52 -0500

    Dijkstra: Rip out ST silliness

--- Dijkstra.hs
+++ Dijkstra.hs
@@ -5,31 +5,25 @@
 module Dijkstra
    ( Graph(..)
    , Distance(..)
-   , dijkstraST
    , dijkstra
    , shortestPaths
    , test
    ) where
 
-import Control.Monad.ST
-import Control.Monad.Trans.Class
 import Data.Ix
 import Data.Monoid
 import Data.Maybe
 import Data.Hashable
-import qualified Data.Array as A
-import qualified Data.Array.MArray as A
-import qualified Data.Array.IArray as A
-import qualified Data.Array.ST as A
 import qualified Data.HashPSQ as PSQ
 import qualified Data.HashMap.Strict as HM
 import Control.Monad.State.Strict
+import Control.Monad.Trans.Class
 
-data Graph n e = Graph (n, n) (HM.HashMap n [(n,e)])
+data Graph n e = Graph (HM.HashMap n [(n,e)])
 
 getNeighbors :: (Eq n, Hashable n)
              => Graph n e -> n -> [(n,e)]
-getNeighbors (Graph _ ns) n = fromMaybe [] $ HM.lookup n ns
+getNeighbors (Graph ns) n = fromMaybe [] $ HM.lookup n ns
 
 data Distance e = Finite !e | Infinite
                 deriving (Show, Eq, Ord)
@@ -38,45 +32,6 @@ instance Monoid e => Monoid (Distance e) where
     mempty = Finite mempty
     Finite a `mappend` Finite b = Finite (a <> b)
     _ `mappend` _               = Infinite
-
-dijkstraST :: forall n e.
-              ( Hashable n, Ix n, Monoid e, Ord e )
-           => Graph n e -> n -> (A.Array n (Distance e), A.Array n n)
-dijkstraST graph@(Graph nodes _) src = runST dijkstraM
-  where
-    dijkstraM :: forall s. ST s (A.Array n (Distance e), A.Array n n)
-    dijkstraM = do
-        dists <- A.newArray nodes Infinite :: ST s (A.STArray s n (Distance e))
-        prevs <- A.newArray_ nodes :: ST s (A.STArray s n n)
-        A.writeArray dists src mempty
-        let vs0 :: PSQ.HashPSQ n (Distance e) ()
-            vs0 = PSQ.singleton src (Finite mempty) ()
-
-            go :: StateT (PSQ.HashPSQ n (Distance e) ()) (ST s) ()
-            go = do
-                mNext <- popPSQ
-                case mNext of
-                  Nothing -> return ()
-                  Just (u, distU, _) ->
-                      forM_ (getNeighbors graph u) $ \(v, len) -> do
-                          let alt = distU <> Finite len
-                          distV <- lift $ A.readArray dists v
-                          when (alt < distV) $ do
-                              lift $ A.writeArray dists v alt
-                              lift $ A.writeArray prevs v u
-                              modify $ PSQ.insert v alt ()
-        runStateT go vs0
-        (,) <$> A.freeze dists <*> A.freeze prevs
-
-popPSQ :: (Monad m, Ord k, Hashable k, Ord p)
-       => StateT (PSQ.HashPSQ k p v) m (Maybe (k, p, v))
-popPSQ = do
-    psq <- get
-    case PSQ.minView psq of
-      Just (k,p,v,rest) -> do put rest
-                              return $ Just (k,p,v)
-      Nothing           -> return Nothing
-
 
 data S n e = S { sAccum :: !(HM.HashMap n (Distance e, [n]))
                , sPSQ   :: !(PSQ.HashPSQ n (Distance e) ())
@@ -143,7 +98,7 @@ shortestPaths paths = go
 
 
 test :: Graph Char (Sum Int)
-test = Graph (a, f) $ HM.fromList
+test = Graph $ HM.fromList
     [ a .= [ b .= 1, c .= 2, d .= 10, e .= 1, f .= 1 ]
     , b .= [ a .= 1, c .= 1 ]
     , c .= [ a .= 2, b .= 1, d .= 1, e .= 1 ]
