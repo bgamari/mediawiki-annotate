820a0b27b6c540d0008d3ad49a42719467b88074
Ben Gamari <ben@smart-cactus.org>
2017-01-18 18:09:07 -0500

    Lazy Dijkstra implementation

--- Dijkstra.hs
+++ Dijkstra.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE MultiWayIf #-}
 
 module Dijkstra
    ( Graph(..)
    , Distance(..)
    , dijkstra
+   , lazyDijkstra
    , shortestPaths
    , test
    ) where
@@ -33,6 +35,9 @@ instance Monoid e => Monoid (Distance e) where
     mempty = Finite mempty
     Finite a `mappend` Finite b = Finite (a <> b)
     _ `mappend` _               = Infinite
+
+--------------------------------------------------
+-- Dijkstra
 
 data S n e = S { sAccum :: !(HM.HashMap n (Distance e, [n]))
                , sPSQ   :: !(PSQ.HashPSQ n (Distance e) ())
@@ -83,6 +88,84 @@ dijkstra graph =
 
               go
 
+----------------------------------------------------------
+-- Lazy Dijkstra
+
+data LS n e = LS { -- | Nodes which we already have a distance-from-source for
+                   lsAccum :: !(HM.HashMap n (Distance e, [n]))
+                   -- | Nodes which we will soon be able to emit distances for
+                 , lsEmit  :: !(PSQ.HashPSQ n (Distance e) [n])
+                   -- | Nodes which we have yet to look at
+                 , lsTodo  :: !(PSQ.HashPSQ n (Distance e) ())
+                 }
+            deriving (Show)
+
+-- | Compute the shortest path lengths and predecessor nodes from the given source node.
+-- By convention the source node has itself as a predecessor.
+--
+-- The resulting paths are lazily constructed: the first elements of the result
+-- will be direct neighbors of the source node, followed by their neighbors,
+-- etc. This allows the consumer to choose how much of the graph they would
+-- like the result to cover.
+lazyDijkstra
+    :: forall n e.
+       ( Hashable n, Monoid e, Ord e, Ord n )
+    => Graph n e -> n -> [(n, Distance e, [n])]
+lazyDijkstra graph =
+    \src -> let s0 = LS { lsAccum = HM.singleton src (Finite mempty, [src])
+                        , lsEmit  = PSQ.empty
+                        , lsTodo  = PSQ.singleton src (Finite mempty) ()
+                        }
+            in go s0
+  where
+    go :: LS n e -> [(n, Distance e, [n])]
+    go s
+      | Just (u, distU, _predsU, todo') <- PSQ.minView $ lsTodo s =
+          let (emitted, lsEmit') = splitLessThan distU (lsEmit s)
+              s' = s { lsTodo = todo'
+                     , lsEmit = lsEmit'
+                     }
+              s'' = foldl' (tryNeighbor (u, distU)) s' (getNeighbors graph u)
+          in emitted ++ go s''
+      | otherwise = PSQ.toList (lsEmit s)
+      where
+        lookupDist :: n -> Distance e
+        lookupDist n = maybe Infinite fst $ HM.lookup n (lsAccum s)
+
+        -- | Probe a neighbor @v@ of @u@
+        tryNeighbor :: (n, Distance e) -> LS n e -> (n, e) -> LS n e
+        tryNeighbor (u, distU) s (v, len)
+          | alt < distV
+          = s { lsAccum = HM.insert v (alt, [u]) (lsAccum s)
+              , lsEmit  = PSQ.insert v alt [u] (lsEmit s)
+              , lsTodo  = PSQ.insert v alt () (lsTodo s)
+              }
+          | alt == distV
+          = let f (Just (_, ns)) = Just (alt, u:ns)
+                f Nothing        = error "lazyDijkstra: impossible"
+            in s { lsAccum = HM.alter f v (lsAccum s)
+                 , lsEmit  = snd $ PSQ.alter (((),) . f) v (lsEmit s)
+                 }
+          | otherwise
+          = s
+          where
+            distV = lookupDist v
+            alt = distU <> Finite len
+
+splitLessThan :: forall n p v. (Ord p, Ord n, Hashable n)
+              => p -> PSQ.HashPSQ n p v -> ([(n,p,v)], PSQ.HashPSQ n p v)
+splitLessThan p0 = go []
+  where
+    go accum psq
+      | Just (k, p, v, psq') <- PSQ.minView psq
+      , p < p0
+      = go ((k,p,v) : accum) psq'
+    go accum psq = (accum, psq)
+
+-------------------------------------------
+-- Path-finding
+
+-- | A path through a graph
 type Path n = [n]
 
 -- | Compute the shortest paths to @n@
@@ -97,6 +180,8 @@ shortestPaths paths = go
            , rest <- if n == m then pure [] else go m    -- check for arrival at source, if not draw remaining path
            ]
 
+-------------------------------------------
+-- Testing
 
 test :: Graph Char (Sum Int)
 test = Graph $ HM.fromList
