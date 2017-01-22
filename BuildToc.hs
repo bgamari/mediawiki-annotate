44d00669c86d415be9ebb47c5811f418c9fb316a
Ben Gamari <ben@smart-cactus.org>
2017-01-21 23:35:29 -0500

    BuildToc: Update for new CBOR

--- BuildToc.hs
+++ BuildToc.hs
@@ -25,22 +25,22 @@ readValuesWithOffsets = start 0 . BSL.toChunks
 
     go :: Offset          -- ^ offset of beginning of current chunk
        -> Offset          -- ^ start offset of thing currently being decoded
-       -> Bin.Decoder a
+       -> CBOR.Decoder a
        -> [BS.ByteString] -- ^ remaining chunks
        -> [(Offset, a)]
-    go !currOff !startOff (Bin.Partial f)   [] =
+    go !currOff !startOff (CBOR.Partial f)   [] =
         go currOff startOff (f Nothing) []
 
-    go currOff  startOff (Bin.Partial f)   (bs:bss) =
+    go currOff  startOff (CBOR.Partial f)   (bs:bss) =
         go currOff startOff (f (Just bs)) bss
 
-    go currOff  startOff (Bin.Done bs off x) bss =
+    go currOff  startOff (CBOR.Done bs off x) bss =
         let !currOff' = currOff + fromIntegral off
             bss' | BS.null bs = bss
                  | otherwise  = bs : bss
         in (startOff, x) : start currOff' bss'
 
-    go _currOff _startOff (Bin.Fail _rest _ err) _ =
+    go _currOff _startOff (CBOR.Fail _rest _ err) _ =
         error $ show err
 
 main :: IO ()
