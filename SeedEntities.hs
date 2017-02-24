70892ddb1c349f109a54594ee8b8079171a0f486
Laura Dietz <dietz@cs.umass.edu>
2017-02-23 22:45:13 -0500

    Add program to filter seed entities

---
+++ SeedEntities.hs
@@ -0,0 +1,25 @@
+import Data.Semigroup hiding (All, Any, option)
+import Options.Applicative
+
+import qualified Data.HashSet as HS
+import qualified Data.ByteString.Lazy as BSL
+
+import CAR.Types
+import GraphExpansionExperiments
+
+opts :: Parser (FilePath)
+opts =
+    id
+    <$> argument str (help "queryFile file" <> metavar "ANNOTATIONS FILE")
+
+main :: IO ()
+main = do
+    (queryFile) <-
+        execParser $ info (helper <*> opts) mempty
+
+    queriesToSeedEntities <- pagesToLeadEntities . decodeCborList <$> BSL.readFile queryFile
+    putStrLn $ unlines $ foldMap toSeedLines $ queriesToSeedEntities
+  where
+    toSeedLines queryDoc =
+           [ (unpackPageId $ queryDocQueryId queryDoc)++" 0 "++ (unpackPageId seed) ++ " 1"
+           | seed <- HS.toList $ queryDocLeadEntities queryDoc]
