06c59832d5c7ee42d794c9df41e3844e8468ac26
Ben Gamari <ben@smart-cactus.org>
2017-05-15 16:57:21 -0400

    fork-map: Fix it
    Here I do a fair bit of refactoring and in the process fix process cleanup.
    
    This took significantly longer than expected due to a rather subtle race
    condition: Debug output from the master process was claiming the MVar associated
    with the stderr Handle while children were being forked. This meant that the
    children ended up hanging as soon as they tried to write their own debug output.
    Because this was correlated to activity on stderr, it ended up being rather
    tricky to track down.
    
    I think ultimately I should finally move away from this forking model.

--- fork-map/src/Control/Concurrent/ForkMap/ForkPipe.hs
+++ fork-map/src/Control/Concurrent/ForkMap/ForkPipe.hs
@@ -23,19 +23,20 @@ import System.Posix.Signals
 import System.Posix.Process
 import Control.Concurrent
 import Control.Concurrent.STM
+import System.Exit
 
 import qualified Data.ByteString.Char8 as BS
 
 import Control.Concurrent.ForkMap.FifoChannel
 
-debug :: Bool
-debug = False
+debugEnabled :: Bool
+debugEnabled = True
 
 -- | A request "inward" from the worker to the worker.
 data InwardMessage a' a b' b r
-    = MasterUpRespond a    -- | upstream node on master has yielded
-    | MasterDownRequest b' -- | downstream node on master has awaited
-    | MasterDone           -- | master has terminated
+    = MasterUpRespond a    -- ^ upstream node on master has yielded
+    | MasterDownRequest b' -- ^ downstream node on master has awaited
+    | MasterDone           -- ^ master has terminated
     deriving (Generic)
 instance (All Binary a' a b' b r) => Binary (InwardMessage a' a b' b r)
 
@@ -46,10 +47,10 @@ showInward  MasterDone = "MasterDone"
 
 -- | A request sent "outward" from the worker to the master
 data OutwardMessage a' a b' b r
-    = WorkerUpRequest a'   -- | worker is waiting for an element from upstream
-    | WorkerDownRespond b  -- | worker has yielded an element downstream
-    | WorkerDone r         -- | worker has terminated gracefully
-    | WorkerFail String    -- | worker has terminated with an exception
+    = WorkerUpRequest a'   -- ^ worker is waiting for an element from upstream
+    | WorkerDownRespond b  -- ^ worker has yielded an element downstream
+    | WorkerDone r         -- ^ worker has terminated gracefully
+    | WorkerFail String    -- ^ worker has terminated with an exception
     deriving (Generic)
 instance (All Binary a' a b' b r) => Binary (OutwardMessage a' a b' b r)
 
@@ -71,89 +72,117 @@ instance Exception WorkerFailException
 
 forkPipe :: forall a' a b' b m n r.
             ( MonadIO (Safe.Base n), Safe.MonadSafe n
-            , All Binary a' a b' b r, MonadIO m)
+            , All Binary a' a b' b r, MonadIO m )
          => (forall x. m x -> IO x)
          -> Proxy a' a b' b m r
          -> n (Proxy a' a b' b n r)
 forkPipe runM pipe = do
     (sendA, recvA) <- liftIO newPipeChannel :: n (Chans (InwardMessage a' a b' b r))
     (sendB, recvB) <- liftIO newPipeChannel :: n (Chans (OutwardMessage a' a b' b r))
-    pid <- liftIO $ forkProcess (worker recvA sendB)
+    pid <- liftIO $ forkProcess (worker runM pipe recvA sendB)
+    debug $ "started worker "++show pid
 
     -- Ensure children are reaped
     exitCode <- liftIO newEmptyTMVarIO
-    _ <- liftIO $ forkIO $ do
-        dbg $ "waiting on "++show pid
-        mx <- getProcessStatus True False pid
-        dbg $ "pid "++show pid++" "++show mx
+    -- _ <- liftIO $ forkIO $ do
+    --     debug $ "waiting on "++show pid
+    --     mx <- eatException $ getProcessStatus True False pid
+    --     debug $ "reaped pid "++show pid++" "++show mx
+    --     case mx of
+    --       Just code -> do
+    --           debug $ "waiting for exit code"
+    --           atomically $ putTMVar exitCode code
+    --           debug "put exit code"
+    --       Nothing   -> return ()
+
+    debug "register"
+    rkey <- Safe.register $ liftIO $ do
+        let term _ = signalProcess sigINT pid
+        debug $ "pid downing "++show pid
+        Safe.handleAll term $ send sendA MasterDone
+        debug $ "sent "++show pid
+        mx <- eatException $ getProcessStatus True False pid
+        debug $ "pid "++show pid++" "++show mx
         case mx of
           Just code -> atomically $ putTMVar exitCode code
           Nothing   -> return ()
 
-    rkey <- Safe.register $ liftIO $ do
-        let term _ = signalProcess sigINT pid
-        dbg $ "pid downing"
-        Safe.handleAll term $ send sendA MasterDone
-        mx <- getProcessStatus True False pid
-        dbg $ "pid "++show pid++" "++show mx
-
     let checkExited exc = liftIO $ do
-            dbg $ "checking exited "++show pid
+            debug $ "checking exited "++show pid
             mcode <- atomically $ tryTakeTMVar exitCode
             case mcode of
               Just code -> throwM $ WorkerKilled code
               Nothing -> throwM exc
+
+    debug "start master"
     return $ handleAll checkExited $ master rkey recvB sendA
   where
-    master :: Safe.ReleaseKey
-           -> ReceiveChan (OutwardMessage a' a b' b r)
-           -> SendChan (InwardMessage a' a b' b r)
-           -> Proxy a' a b' b n r
-    master rkey recvC sendC = go
-      where
-        go = do
-            req <- liftIO $ receive recvC
-            liftIO $ dbg $ "master: "++showOutward req
-            case req of
-              WorkerUpRequest   a' -> P.Request a' goRequest
-              WorkerDownRespond b  -> P.Respond b  goRespond
-              WorkerDone        r  -> Safe.release rkey >> pure r
-              WorkerFail        e  -> Safe.release rkey >> throwM (WorkerFailed e)
-        goRequest a  = liftIO (send sendC (MasterUpRespond a)) >> go
-        goRespond b' = liftIO (send sendC (MasterDownRequest b')) >> go
+    eatException = id --Safe.handleAll (\e -> debug ("uh oh! "++show e) >> throw e)
 
-    worker :: ReceiveChan (InwardMessage a' a b' b r)
-           -> SendChan (OutwardMessage a' a b' b r)
-           -> IO ()
-    worker recvC sendC = do
-        setNumCapabilities 1
-        Safe.handle onError $ runM $ go pipe
-      where
-        onError (SomeException e) = do
-            liftIO $ dbg $ "Worker: "++show e
-            send sendC (WorkerFail $ show e)
+master :: forall a' a b' b n r.
+          ( MonadSafe n, All Binary a' a b' b r )
+       => Safe.ReleaseKey
+       -> ReceiveChan (OutwardMessage a' a b' b r)
+       -> SendChan (InwardMessage a' a b' b r)
+       -> Proxy a' a b' b n r
+master rkey recvC sendC = go
+  where
+    go = do
+        req <- liftIO $ receive recvC
+        debug $ "master: "++showOutward req
+        case req of
+          WorkerUpRequest   a' -> P.Request a' goRequest
+          WorkerDownRespond b  -> P.Respond b  goRespond
+          WorkerDone        r  -> Safe.release rkey >> pure r
+          WorkerFail        e  -> Safe.release rkey >> throwM (WorkerFailed e)
+    goRequest a  = liftIO (send sendC (MasterUpRespond a)) >> go
+    goRespond b' = liftIO (send sendC (MasterDownRequest b')) >> go
 
-        go (P.Request a' cont) = do
-            liftIO $ dbg "worker: Request"
-            liftIO $ send sendC (WorkerUpRequest a')
-            reply <- liftIO $ receive recvC
-            liftIO $ dbg $ showInward reply
-            case reply of
-              MasterUpRespond   a -> go $ cont a
-              MasterDownRequest _ -> error "Worker saw MasterDownRequest"
-              MasterDone          -> return ()
-        go (P.Respond b cont) = do
-            liftIO $ dbg "worker: Respond"
-            liftIO $ send sendC (WorkerDownRespond b)
-            reply <- liftIO $ receive recvC
-            liftIO $ dbg $ showInward reply
-            case reply of
-              MasterUpRespond   _  -> error "Worker saw MasterUpRespond"
-              MasterDownRequest b' -> go $ cont b'
-              MasterDone           -> return ()
-        go (P.M m) = m >>= go
-        go (P.Pure r) = liftIO $ send sendC (WorkerDone r)
+worker :: forall a' a b' b m r.
+            ( All Binary a' a b' b r, MonadIO m )
+       => (forall x. m x -> IO x)
+       -> Proxy a' a b' b m r
+       -> ReceiveChan (InwardMessage a' a b' b r)
+       -> SendChan (OutwardMessage a' a b' b r)
+       -> IO ()
+worker runM pipe recvC sendC = do
+    setNumCapabilities 1
+    dbg "started"
+    Safe.handle onError $ runM $ go pipe
+    liftIO $ exitImmediately ExitSuccess
+  where
+    onError (SomeException e) = do
+        dbg $ "exception: "++show e
+        send sendC (WorkerFail $ show e)
 
-dbg :: MonadIO m => String -> m ()
-dbg x | debug = liftIO $ BS.hPutStrLn stderr $ BS.pack x
-dbg _ = return ()
+    dbg s = return ()
+    dbg s = liftIO $ do
+        pid <- getProcessID
+        debug $ "Worker("++show pid++"): "++s
+
+    go (P.Request a' cont) = do
+        dbg "Request"
+        liftIO $ send sendC (WorkerUpRequest a')
+        reply <- liftIO $ receive recvC
+        liftIO $ dbg $ showInward reply
+        case reply of
+          MasterUpRespond   a -> go $ cont a
+          MasterDownRequest _ -> fail "Worker saw MasterDownRequest"
+          MasterDone          -> exitNow
+    go (P.Respond b cont) = do
+        dbg "Respond"
+        liftIO $ send sendC (WorkerDownRespond b)
+        reply <- liftIO $ receive recvC
+        liftIO $ dbg $ showInward reply
+        case reply of
+          MasterUpRespond   _  -> fail "Worker saw MasterUpRespond"
+          MasterDownRequest b' -> go $ cont b'
+          MasterDone           -> exitNow
+    go (P.M m) = dbg "Effect" >> m >>= go
+    go (P.Pure r) = liftIO $ send sendC (WorkerDone r)
+
+    exitNow = dbg "exiting"
+
+debug :: MonadIO m => String -> m ()
+debug x | debugEnabled = liftIO $ BS.hPutStrLn stderr $ BS.pack x
+debug _ = return ()
