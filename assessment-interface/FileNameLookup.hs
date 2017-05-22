cda4a8b373981933ef3f9958b6c049e9a8591e9f
Laura Dietz <dietz@cs.umass.edu>
2017-05-21 21:06:56 -0400

    write files percent-encoded, but do not encode links (the browser will percent encode them)

--- assessment-interface/FileNameLookup.hs
+++ assessment-interface/FileNameLookup.hs
@@ -40,7 +40,7 @@ fileNameLookupFactory existResultsForSectionpath  = FileNameLookup {..}
   where
     outlinePathname :: Stub -> FilePath
     outlinePathname (Stub _ pageId _) =
-       (unpacgit kPageId pageId) </> "index" <.> "html"   -- todo I think switching from pageId to pageName get's rid of our url encoding issues
+       (unpackPageId pageId) </> "index" <.> "html"   -- todo I think switching from pageId to pageName get's rid of our url encoding issues
 
     outlineURL :: Stub -> FilePath
     outlineURL stub =
