0a4fe24300a5c7512f1636df923cfa43b264325f
Ben Gamari <ben@smart-cactus.org>
2017-06-01 16:06:10 -0400

    Types: Add readCborList utility

--- src/CAR/Types.hs
+++ src/CAR/Types.hs
@@ -27,9 +27,8 @@ module CAR.Types
     , LinkStyle
     , withLink, anchorOnly
       -- * Miscellaneous Utilities
-    , decodeCborList
-    , encodeCborList
-    , writeCborList
+    , decodeCborList, readCborList
+    , encodeCborList, writeCborList
     ) where
 
 import Data.Foldable
@@ -193,6 +192,9 @@ decodeCborList = \bs -> runST $ start $ BSL.toChunks bs
               rest <- unsafeInterleaveST $ start (bs : bss)
               return (x : rest)
           (CBOR.Fail _rest _ err, _) -> error $ show err
+
+readCborList :: CBOR.Serialise a => FilePath -> IO [a]
+readCborList path = decodeCborList <$> BSL.readFile path
 
 encodeCborList :: CBOR.Serialise a => [a] -> BSB.Builder
 encodeCborList = CBOR.toBuilder . foldMap CBOR.encode
