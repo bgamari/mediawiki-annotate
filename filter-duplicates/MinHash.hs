82c36bf3c7f5049ee327d7e4e5ca50bab341ccc4
Ben Gamari <ben@smart-cactus.org>
2017-06-03 16:30:31 -0400

    MinHash: A bit of refactoring

--- filter-duplicates/MinHash.hs
+++ filter-duplicates/MinHash.hs
@@ -50,6 +50,13 @@ instance Monoid (Bag a) where
     mempty = None
     mappend = Two
 
+type Projections n = [WordVec n]
+
+-- | Generate @n@ random splitting hyperplanes
+randomProjections :: KnownNat n => Int -> IO (Projections n)
+randomProjections nProjs = withSystemRandom $ asGenST $ \gen ->
+    replicateM nProjs $ generateWordVec (const $ fmap realToFrac $ standard gen)
+
 -- | Partition paragraphs into buckets via random projections of the accumulated
 -- word embedding vector.
 partitionParas :: forall n. KnownNat n => Projections n
@@ -77,16 +84,11 @@ partitionParas projs paras =
     strat :: Strategy [M.Map Bucket (Bag (ParagraphId, [Term]))]
     strat = parBuffer 256 $ evalTraversable $ evalTraversable r0
 
-type Projections n = [WordVec n]
-
-randomProjections :: KnownNat n => Int -> IO (Projections n)
-randomProjections nProjs = withSystemRandom $ asGenST $ \gen -> do
-    replicateM nProjs $ generateWordVec (const $ fmap realToFrac $ standard gen)
-
 -- | Given a list of projections of an n-dimensional embedding space and a word
 -- vector (such as accumulated word vectors of word in a paragraph), compute the
 -- 'Bucket' to which the vector belongs.
-bucketForPara :: KnownNat n => Projections n -> WordVec n -> Bucket
+bucketForPara :: (KnownNat n)
+              => Projections n -> WordVec n -> Bucket
 bucketForPara projs v =
     let fromBits :: [Bool] -> Bucket
         fromBits = Bucket . foldl' (.|.) 0 . zipWith toBit [0..]
@@ -95,6 +97,46 @@ bucketForPara projs v =
             toBit n True  = bit n
             toBit n False = 0
     in fromBits $ map (\p -> dotWordVecs v p > 1) projs
+
+-- | Mean-center word embeddings
+centerWordEmbedding :: (KnownNat n)
+                    => V.Vector (ParagraphId, [Term], WordVec n)
+                    -> V.Vector (ParagraphId, [Term], WordVec n)
+centerWordEmbedding uncenteredParas =
+    V.map center uncenteredParas
+  where
+    center (pid, terms, v) = (pid, terms, subtractWordVec embeddingMean v)
+    !embeddingMean =
+        scaleWordVec (recip $ realToFrac $ V.length uncenteredParas)
+        $ sumWordVecs
+        $ withStrategy (parBuffer 256 rseq)
+        $ map (sumWordVecs . map (\(_,_,v) -> v) . V.toList)
+        $ chunksOf 100000 uncenteredParas
+
+-- | For all elements of a given bucket, compute the similarity and chunks which
+-- we will parallelise pair-wise similarity computation over. (paras are all from
+-- the same bucket). Only pairs with sufficintly high jaccard will be returned.
+-- (Needs final testing on real bigrams)
+hashSimilarities :: Double -> V.Vector (ParagraphId, [Term]) -> [[(ParagraphId, ParagraphId)]]
+hashSimilarities thresh paras =
+    [ [ (pid1, pid2)
+      | (pid1, bigrams1) <- V.toList chunk
+      , (pid2, bigrams2) <- takeWhile (\(pid,_) -> pid < pid1) $ V.toList bigramHashes
+      , let (denom, num) = IS.unionIntersectSize bigrams1 bigrams2
+            sim = realToFrac num / realToFrac denom
+      , sim > thresh
+      ]
+    | chunk <- chunksOf 100 bigramHashes
+    ]
+  where
+    toBigramHashes :: [Term] -> IS.IntSet
+    toBigramHashes toks =
+        IS.fromAscList $ sort
+        $ map hash $ toBigrams toks
+    -- for each paragraph: bigrams are hashed onto integers
+    bigramHashes :: V.Vector (ParagraphId, IS.IntSet)
+    !bigramHashes = fmap (fmap toBigramHashes) paras
+
 
 opts :: Parser (FilePath, Double, Int, FilePath, FilePath)
 opts = (,,,,)
@@ -121,24 +163,14 @@ main = do
     putStrLn "Read embeddings"
     setNumCapabilities ncaps
 
-    let embeddedParas :: V.Vector (ParagraphId, [Term], WordVec n)
-        embeddedParas = V.map embed uncenteredParas
-          where
-            embed (pid, terms, v) =
-                (pid, terms, subtractWordVec embeddingMean v)
+    let !embeddedParas = centerWordEmbedding uncenteredParas
 
         uncenteredParas :: V.Vector (ParagraphId, [Term], WordVec n)
         uncenteredParas = V.map embed paras
           where
             embed (pid, terms) = (pid, terms, embedTerms embedding terms)
 
-        !embeddingMean =
-            scaleWordVec (recip $ realToFrac $ V.length paras)
-            $ sumWordVecs
-            $ withStrategy (parBuffer 256 rseq)
-            $ map (sumWordVecs . map (\(_,_,v) -> v) . V.toList)
-            $ chunksOf 100000 uncenteredParas
-    print $ wordVecElems embeddingMean
+    putStrLn "Computed embedding"
 
     let partitions :: M.Map Bucket (V.Vector (ParagraphId, [Term]))
         !partitions = partitionParas projs embeddedParas
@@ -149,27 +181,3 @@ main = do
         duplicates = concat $ listStatus "dup-chunk" 1000 $ withStrategy (parBuffer 1024 rdeepseq)
                      $ foldMap (hashSimilarities thresh) $ M.elems partitions
     writeFile outputFile $ show duplicates
-
--- | For all elements of a given bucket, compute the similarity and chunks which
--- we will parallelise pair-wise similarity computation over. (paras are all from
--- the same bucket). Only pairs with sufficintly high jaccard will be returned.
--- (Needs final testing on real bigrams)
-hashSimilarities :: Double -> V.Vector (ParagraphId, [Term]) -> [[(ParagraphId, ParagraphId)]]
-hashSimilarities thresh paras =
-    [ [ (pid1, pid2)
-      | (pid1, bigrams1) <- V.toList chunk
-      , (pid2, bigrams2) <- takeWhile (\(pid,_) -> pid < pid1) $ V.toList bigramHashes
-      , let (denom, num) = IS.unionIntersectSize bigrams1 bigrams2
-            sim = realToFrac num / realToFrac denom
-      , sim > thresh
-      ]
-    | chunk <- chunksOf 100 bigramHashes
-    ]
-  where
-    toBigramHashes :: [Term] -> IS.IntSet
-    toBigramHashes toks =
-        IS.fromAscList $ sort
-        $ map hash $ toBigrams toks
-    -- for each paragraph: bigrams are hashed onto integers
-    bigramHashes :: V.Vector (ParagraphId, IS.IntSet)
-    !bigramHashes = fmap (fmap toBigramHashes) paras
