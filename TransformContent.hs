707d4a7c0859ed4215d22f5f0545d7823b79b3c3
Laura Dietz <dietz@cs.umass.edu>
2017-06-09 21:20:22 -0400

    TransformContent has mode for only filtering forbidden sections and categories

--- TransformContent.hs
+++ TransformContent.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 
 import Data.Monoid hiding (All, Any)
 import System.IO
@@ -26,11 +27,16 @@ helpDescr =
         cmd "need doc"                           "here"
       ]
 
-opts :: Parser (FilePath, FilePath)
+opts :: Parser (FilePath, FilePath, Page -> Maybe Page)
 opts =
-    (,)
+    (,,)
     <$> argument str (help "input file" <> metavar "ANNOTATIONS FILE")
     <*> option str (short 'o' <> long "output" <> metavar "FILE" <> help "Output file")
+    <*> (fullMode <|> sectionsCategoriesMode)
+  where 
+    fullMode = flag' fullTransformContent (long "full" <> help "full filtering of lead, category, hidden sections, and short page remainders")
+    sectionsCategoriesMode = flag' transformCategoriesAndForbiddenSection  (long "sections-categories" <> help "partial filtering of category and hidden sections only")
+
     -- <*> argument predicate (metavar "PRED" <> help "Predicate")
 
 forbiddenHeadings :: HS.HashSet T.Text
@@ -38,10 +44,16 @@ forbiddenHeadings = HS.fromList ["see also", "references", "external links", "no
     "bibliography", "gallery", "publications", "further reading", "track listing", "sources",
     "cast", "discography", "awards", "other"]
 
-isLead :: PageSkeleton -> Bool
-isLead (Para{})    = True
-isLead (Section{}) = False
-isLead (Image{})   = False
+isPara :: PageSkeleton -> Bool
+isPara (Para{})    = True
+isPara (Section{}) = False
+isPara (Image{})   = False
+
+isImage :: PageSkeleton -> Bool
+isImage (Para{})    = False
+isImage (Section{}) = False
+isImage (Image{})   = True
+
 
 isForbiddenSkeleton :: PageSkeleton -> Bool
 isForbiddenSkeleton (Section heading _ _) =
@@ -51,29 +63,111 @@ isForbiddenSkeleton (Section heading _ _) =
 isForbiddenSkeleton (Image _ _) = True
 isForbiddenSkeleton _ = False
 
-recurseDropForbiddenSections :: PageSkeleton -> Maybe PageSkeleton
-recurseDropForbiddenSections (Section heading sectionId children)
+-- recurseDropForbiddenSections =  recurseSections (not . isForbiddenSkeleton)
+
+recurseFilterSections :: (PageSkeleton -> Bool) -> PageSkeleton -> Maybe PageSkeleton
+recurseFilterSections f (Section heading sectionId children)
     | null children' = Nothing
     | otherwise =  Just (Section heading sectionId children')
-  where children' = mapMaybe recurseDropForbiddenSections $ filter (not . isForbiddenSkeleton) children
-recurseDropForbiddenSections x = Just x
+  where children' = mapMaybe (recurseFilterSections f) $ filter f children
+recurseFilterSections f x = Just x
 
 
-transformContent :: Page -> Maybe Page
-transformContent (Page {pageName, pageId, pageSkeleton=pageSkeleta})
-  | length pageSkeleta' > 3 =
-      Just (Page pageName pageId pageSkeleta')
-  | otherwise = Nothing
+recurseFilterPage :: (PageSkeleton -> Bool) -> Page -> Page
+recurseFilterPage f (Page {..}) =
+    Page pageName pageId children'
+  where children' = mapMaybe (recurseFilterSections f) pageSkeleton
+
+isCategoriesPara :: PageSkeleton -> Bool
+isCategoriesPara (Para (Paragraph paraId paraBody)) =
+    all isCategoryLinks paraBody
   where
-    pageSkeleta'=
-         mapMaybe recurseDropForbiddenSections
-         $ filter (\skel -> not (isForbiddenSkeleton skel) &&  not (isLead skel)) pageSkeleta
+    isCategoryLinks :: ParaBody -> Bool
+    isCategoryLinks (ParaText _) = False
+    isCategoryLinks (ParaLink (Link (PageName name) section _ anchor)) =
+      "Category:" `T.isPrefixOf` name
+isCategoriesPara _ = False
+
+-- transformOnlySections :: Page -> Page
+-- transformOnlySections (Page {pageName, pageId, pageSkeleton=pageSkeleta}) =
+--     Page pageName pageId pageSkeleta'
+--   where
+--     pageSkeleta'=
+--          mapMaybe recurseDropForbiddenSections
+--          $ filter (\skel -> not (isForbiddenSkeleton skel || isCategoriesPara skel)) pageSkeleta
+--
+-- transformContent :: Page -> Maybe Page
+-- transformContent (Page {pageName, pageId, pageSkeleton=pageSkeleta})
+--   | length pageSkeleta' > 3 =
+--       Just (Page pageName pageId pageSkeleta')
+--   | otherwise = Nothing
+--   where
+--     pageSkeleta'=
+--          mapMaybe recurseDropForbiddenSections
+--          $ filter (\skel -> not (isForbiddenSkeleton skel) &&  not (isLead skel))
+--            pageSkeleta
+
+---
+
+--
+-- transformForbiddenSections :: Page -> Page
+-- transformForbiddenSections (Page {pageName, pageId, pageSkeleton=pageSkeleta}) =
+--     Page pageName pageId pageSkeleta'
+--   where
+--     pageSkeleta'=
+--          mapMaybe recurseDropForbiddenSections
+--          $ filter (\skel -> not (isForbiddenSkeleton skel))
+--            pageSkeleta
+--
+--
+--
+-- transformDropLead :: Page -> Page
+-- transformDropLead (Page {pageName, pageId, pageSkeleton=pageSkeleta}) =
+--     Page pageName pageId pageSkeleta'
+--   where
+--     pageSkeleta'=
+--          filter (\skel -> not (isLead skel))
+--            pageSkeleta
 
 
+filterTopLevel :: (PageSkeleton -> Bool) -> Page  ->  Page
+filterTopLevel f (Page {pageName, pageId, pageSkeleton=pageSkeleta}) =
+    Page pageName pageId (filter f pageSkeleta)
+
+
+
+dropShortPage :: Page -> Maybe Page
+dropShortPage page@(Page {pageName, pageId, pageSkeleton=pageSkeleta})
+  | length pageSkeleta > 3 =
+      Just page
+  | otherwise = Nothing
+  
+
+fullTransformContent :: Page -> Maybe Page
+fullTransformContent page =
+    dropShortPage
+    $ recurseFilterPage (not . isForbiddenSkeleton)
+    $ recurseFilterPage (not . isImage)
+    $ filterTopLevel (not . isCategoriesPara)
+    $ filterTopLevel (not . isPara)
+    page
+
+
+
+transformCategoriesAndForbiddenSection :: Page -> Maybe Page
+transformCategoriesAndForbiddenSection page =
+    Just
+    $ recurseFilterPage (not . isForbiddenSkeleton)
+    $ recurseFilterPage (not . isImage)
+    $ filterTopLevel (not . isCategoriesPara)
+    page
+
+---
 
 main :: IO ()
 main = do
-    (inputFile, outputFile) <- execParser $ info (helper <*> opts) (progDescDoc $ Just helpDescr)
+    (inputFile, outputFile, transformMode) <- execParser $ info (helper <*> opts) (progDescDoc $ Just helpDescr)
     pages <- decodeCborList <$> BSL.readFile inputFile
     withFile outputFile WriteMode $ \h ->
-        BSB.hPutBuilder h $ encodeCborList $ mapMaybe transformContent $ pages
+        BSB.hPutBuilder h $ encodeCborList $ mapMaybe transformMode pages
+
