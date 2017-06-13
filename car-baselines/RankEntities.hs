093fbd2bdc36093fc7b38252c97d5d16be72f830
Ben Gamari <ben@smart-cactus.org>
2017-06-13 01:10:52 -0400

    RankEntities: Produce proper ranking

--- car-baselines/RankEntities.hs
+++ car-baselines/RankEntities.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import Data.Bifunctor
 import Data.List
@@ -21,30 +22,43 @@ instance Monoid EntityCounts where
     EntityCounts a `mappend` EntityCounts b = EntityCounts $ M.unionWith (<>) a b
 
 
-queryEntities :: (ParagraphId -> Paragraph) -> Seq.Seq Run.RankingEntry -> [(PageId, ParagraphId)]
+queryEntities :: (ParagraphId -> Paragraph) -> Seq.Seq Run.RankingEntry -> [(PageId, ParagraphId, Run.Score)]
 queryEntities lookupPara ranking =
     let EntityCounts counts = foldMap countEntities ranking
         entityRanking :: [(PageId, (Max (Run.Score, ParagraphId)))]
         entityRanking = map (second snd) $ sortBy (flip $ comparing $ fst . snd) $ M.toList counts
-    in fmap (fmap $ snd . getMax) entityRanking
+    in [ (pageId, paraId, score)
+       | (pageId, (Max (score, paraId))) <- entityRanking
+       ]
   where
     countEntities :: Run.RankingEntry -> EntityCounts
     countEntities r = foldMap (toCounts . linkTargetId) (paraLinks para)
       where
         toCounts target =
             EntityCounts $ M.singleton target (Sum 1, Max (Run.carScore r, paraId para))
-        para = lookupPara $ Run.carPassage r
+        para = lookupPara $ fromMaybe (error "No paragraph") $ Run.carPassage r
 
-opts :: Parser (FilePath, TocFile.IndexedCborPath ParagraphId Paragraph)
-opts = (,)
-    <$> option str (short 'r' <> long "ranking" <> help "TREC ranking file")
+opts :: Parser (FilePath,  FilePath, TocFile.IndexedCborPath ParagraphId Paragraph)
+opts = (,,)
+    <$> option str (short 'o' <> long "output" <> help "Output TREC ranking file")
+    <*> option str (short 'r' <> long "ranking" <> help "TREC ranking file")
     <*> option (TocFile.IndexedCborPath <$> str) (short 'p' <> long "paragraphs" <> help "Paragraphs file")
 
 main :: IO ()
 main = do
-    (runFile, parasFile) <- execParser $ info (helper <*> opts) mempty
+    (outputFile, runFile, parasFile) <- execParser $ info (helper <*> opts) mempty
     queries <- Run.groupByQuery <$> Run.readRunFile runFile
     paras <- TocFile.open parasFile
     let lookupPara = fromMaybe (error "uh oh") . flip TocFile.lookup paras
-    print $ fmap (queryEntities lookupPara) queries
-
+    --print $ fmap (queryEntities lookupPara) queries
+    Run.writeRunFile outputFile
+        [ Run.RankingEntry { Run.carQueryId = qid
+                           , Run.carPassage = Just paraId
+                           , Run.carEntity  = Just pageId
+                           , Run.carRank    = rank
+                           , Run.carScore   = score
+                           , Run.carMethodName = Run.MethodName "by+entity"
+                           }
+        | (qid, queryRanking) <- M.toList queries
+        , (rank, (pageId, paraId, score)) <- zip [1..] $ queryEntities lookupPara queryRanking
+        ]
