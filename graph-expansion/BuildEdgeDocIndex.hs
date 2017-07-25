181b42182f22cfaf50301e52d7a767d36a4f91a0
Ben Gamari <ben@smart-cactus.org>
2017-07-24 22:15:41 -0400

    graph-expansion: Add query mode

--- graph-expansion/BuildEdgeDocIndex.hs
+++ graph-expansion/BuildEdgeDocIndex.hs
@@ -1,19 +1,40 @@
 import Control.Applicative
+import Data.Foldable
+import Data.Maybe
 import Data.Monoid
 
 import Options.Applicative
 import EdgeDocCorpus
 import EdgeDocIndex
 import CAR.Types
+import SimplIR.Term as Term
+import SimplIR.DiskIndex as DiskIndex
 
-args :: Parser (FilePath, FilePath)
-args = (,)
-    <$> option str (long "output" <> short 'o' <> help "output index path")
-    <*> argument str (help "articles file")
+modes :: Parser (IO ())
+modes = subparser
+    $ command "build" (info buildMode mempty)
+   <> command "query" (info queryMode mempty)
+  where
+    buildMode =
+        go <$> option str (long "output" <> short 'o' <> help "output index path")
+           <*> argument str (help "articles file")
+      where
+        go outputPath articlesPath = do
+            pages <- readCborList articlesPath
+            _ <- indexEdgeDocs outputPath (pagesToEdgeDocs pages)
+            return ()
+
+    queryMode =
+        go <$> option (OnDiskIndex <$> str) (long "index" <> short 'i' <> help "index path")
+           <*> some (argument (Term.fromString <$> str) (help "query terms"))
+      where
+        go :: OnDiskIndex DocMeta Int -> [Term] -> IO ()
+        go indexPath terms = do
+            idx <- DiskIndex.openOnDiskIndex indexPath
+            let postings = fold $ mapMaybe (`DiskIndex.lookupPostings` idx) terms
+            print postings
 
 main :: IO ()
 main = do
-    (outputPath, articlesPath) <- execParser $ info (helper <*> args) mempty
-    pages <- readCborList articlesPath
-    _ <- indexEdgeDocs outputPath (pagesToEdgeDocs pages)
-    return ()
+    mode <- execParser $ info (helper <*> modes) mempty
+    mode
