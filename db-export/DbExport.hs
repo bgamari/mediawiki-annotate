24fe8b150d1e32adf6dfd78503d7f26357f3efec
Ben Gamari <ben@smart-cactus.org>
2017-08-19 07:08:40 -0400

    DbExport: More views

--- db-export/DbExport.hs
+++ db-export/DbExport.hs
@@ -42,29 +42,56 @@ createSchema =
                ( id serial PRIMARY KEY
                , title text NOT NULL
                , parent integer REFERENCES fragments (id)
-               ) |]
+               )
+      |]
     , [sql| CREATE TABLE IF NOT EXISTS paragraphs
                ( id serial PRIMARY KEY
                , paragraph_id text NOT NULL
                , fragment integer REFERENCES fragments (id)
                , content text
                , content_index tsvector
-               ) |]
+               )
+      |]
     , [sql| CREATE TABLE IF NOT EXISTS links
                ( src_fragment integer NOT NULL REFERENCES fragments (id)
                , dest_fragment integer NOT NULL REFERENCES fragments (id)
                , paragraph integer NOT NULL REFERENCES paragraphs (id)
                , anchor text
-               ) |]
+               )
+      |]
+    , [sql| CREATE VIEW raw_fragment_parents(fragment_id, parent, depth) AS
+            WITH RECURSIVE frags(fragment_id, parent, depth) AS (
+                SELECT fragments.id AS fragment_id,
+                      fragments.parent AS parent,
+                      0 AS depth
+                FROM fragments
+                WHERE fragments.parent IS NOT NULL
+              UNION ALL
+                SELECT frags.fragment_id, parent.parent, frags.depth+1
+                FROM frags, fragments AS parent
+                WHERE frags.parent = parent.id
+                  AND parent.parent IS NOT NULL
+            )
+            SELECT * FROM frags
+      |]
+    , [sql| CREATE VIEW fragment_parents(fragment_id, title, parent_ids, parent_titles) AS
+                SELECT fragment_id,  fragments.title, array_agg(parent.id) AS parent_ids, array_agg(parent.title) AS parent_titles
+                FROM raw_fragment_parents
+                INNER JOIN fragments ON fragments.id = raw_fragment_parents.fragment_id
+                INNER JOIN fragments AS parent ON parent.id = raw_fragment_parents.parent
+                WHERE fragment_id=12563
+                GROUP BY raw_fragment_parents.fragment_id, fragments.title
+      |]
     , [sql| CREATE VIEW links_view AS
             SELECT src.title AS src_title,
                    dest.title AS dest_title,
-
+                   paragraphs.content AS paragraph,
                    anchor
-            FROM links, fragments AS src, fragments AS dest
+            FROM links, fragments AS src, fragments AS dest, paragraphs
             WHERE links.src_fragment = src.id
               AND links.dest_fragment = dest.id
-            |]
+              AND paragraphs.id = links.paragraph
+      |]
     , [sql| CREATE INDEX ON fragments (title)  |]
     , [sql| CREATE INDEX ON paragraphs (paragraph_id)  |]
     , [sql| CREATE INDEX ON paragraphs (content_index)  |]
