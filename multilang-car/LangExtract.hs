56f769e641b1d9e6605849c9a280df85a8c35be0
Ben Gamari <ben@smart-cactus.org>
2017-08-26 13:11:06 -0400

    multilang-car: Split out types

--- multilang-car/LangExtract.hs
+++ multilang-car/LangExtract.hs
@@ -1,78 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DeriveGeneric #-}
 
-import GHC.Generics
-import Data.Aeson
-import Data.Hashable
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.HashMap.Strict as HM
-import qualified Data.Text as T
-import qualified Data.Text.Read as TR
 import qualified Codec.Serialise as CBOR
-
 import qualified Data.JsonStream.Parser as JS
+import WikiData
 
-newtype Lang = Lang T.Text
-             deriving (Show, Eq, Ord, Hashable, FromJSON, FromJSONKey, CBOR.Serialise)
-
-newtype ItemId = ItemId Int
-                 deriving (Show, Eq, Ord, Hashable, CBOR.Serialise)
-
-instance FromJSON ItemId where
-    parseJSON = withText "item id" $ maybe (fail "invalid item id") pure . readItemId
-
-readItemId :: T.Text -> Maybe ItemId
-readItemId s
-  | Just rest <- T.stripPrefix "Q" $ T.strip s
-  , Right (n, _) <- TR.decimal rest
-  = Just $ ItemId n
-  | otherwise
-  = Nothing
-
-newtype SiteId = SiteId T.Text
-               deriving (Show, Eq, Ord, Hashable, FromJSON, ToJSON, FromJSONKey, CBOR.Serialise)
-
-newtype PageName = PageName T.Text
-               deriving (Show, Eq, Ord, Hashable, FromJSON, ToJSON, CBOR.Serialise)
-
-data EntityType = Item
-                deriving (Show, Generic)
-instance CBOR.Serialise EntityType
-instance FromJSON EntityType where
-    parseJSON = withText "entity type" $ \s ->
-      case s of
-        "item" -> return Item
-        _      -> fail "unknown entity type"
-
--- | A projection of the wikidata entity representation.
-data Entity = Entity { entityType :: EntityType
-                     , entityId   :: ItemId
-                     , entityLabels :: [(Lang, T.Text)]
-                     , entitySiteLinks :: [(SiteId, PageName)]
-                     }
-            deriving (Show, Generic)
-instance CBOR.Serialise Entity
-
-instance FromJSON Entity where
-    parseJSON = withObject "entity" $ \o ->
-        Entity <$> o .: "type"
-               <*> o .: "id"
-               <*> (o .: "labels" >>= withObject "labels" parseLabels)
-               <*> (o .: "sitelinks" >>= withObject "site links" parseSitelinks)
-      where
-        parseLabels = mapM parseLabel . HM.elems
-        parseLabel = withObject "label" $ \o ->
-              (,) <$> o .: "language"
-                  <*> o .: "value"
-
-        parseSitelinks = mapM parseSitelink . HM.elems
-        parseSitelink = withObject "site link" $ \o ->
-              (,) <$> o .: "site"
-                  <*> o .: "title"
-
-buildIndex :: BSL.ByteString -> HM.HashMap ItemId (HM.HashMap SiteId PageName)
+buildIndex :: BSL.ByteString -> LangIndex
 buildIndex =
     foldMap f . JS.parseLazyByteString (JS.arrayOf (JS.value @Entity))
   where
@@ -84,4 +19,4 @@ buildIndex =
 main :: IO ()
 main = do
     --BSL.getContents >>= print . eitherDecode @[Entity]
-    BSL.getContents >>= pure . buildIndex >>= BSL.writeFile "out" . CBOR.serialise
+    BSL.getContents >>= pure . buildIndex >>= BSL.writeFile "language-index.cbor" . CBOR.serialise
---
+++ multilang-car/src/WikiData.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module WikiData where
+
+import GHC.Generics
+import Data.Aeson
+import Data.Hashable
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Data.Text.Read as TR
+import qualified Codec.Serialise as CBOR
+
+type LangIndex = HM.HashMap ItemId (HM.HashMap SiteId PageName)
+
+newtype Lang = Lang T.Text
+             deriving (Show, Eq, Ord, Hashable, FromJSON, FromJSONKey, CBOR.Serialise)
+
+newtype ItemId = ItemId Int
+                 deriving (Show, Eq, Ord, Hashable, CBOR.Serialise)
+
+instance FromJSON ItemId where
+    parseJSON = withText "item id" $ maybe (fail "invalid item id") pure . readItemId
+
+readItemId :: T.Text -> Maybe ItemId
+readItemId s
+  | Just rest <- T.stripPrefix "Q" $ T.strip s
+  , Right (n, _) <- TR.decimal rest
+  = Just $ ItemId n
+  | otherwise
+  = Nothing
+
+newtype SiteId = SiteId T.Text
+               deriving (Show, Eq, Ord, Hashable, FromJSON, ToJSON, FromJSONKey, CBOR.Serialise)
+
+newtype PageName = PageName T.Text
+               deriving (Show, Eq, Ord, Hashable, FromJSON, ToJSON, CBOR.Serialise)
+
+data EntityType = Item
+                deriving (Show, Generic)
+instance CBOR.Serialise EntityType
+instance FromJSON EntityType where
+    parseJSON = withText "entity type" $ \s ->
+      case s of
+        "item" -> return Item
+        _      -> fail "unknown entity type"
+
+-- | A projection of the wikidata entity representation.
+data Entity = Entity { entityType :: EntityType
+                     , entityId   :: ItemId
+                     , entityLabels :: [(Lang, T.Text)]
+                     , entitySiteLinks :: [(SiteId, PageName)]
+                     }
+            deriving (Show, Generic)
+instance CBOR.Serialise Entity
+
+instance FromJSON Entity where
+    parseJSON = withObject "entity" $ \o ->
+        Entity <$> o .: "type"
+               <*> o .: "id"
+               <*> (o .: "labels" >>= withObject "labels" parseLabels)
+               <*> (o .: "sitelinks" >>= withObject "site links" parseSitelinks)
+      where
+        parseLabels = mapM parseLabel . HM.elems
+        parseLabel = withObject "label" $ \o ->
+              (,) <$> o .: "language"
+                  <*> o .: "value"
+
+        parseSitelinks = mapM parseSitelink . HM.elems
+        parseSitelink = withObject "site link" $ \o ->
+              (,) <$> o .: "site"
+                  <*> o .: "title"
