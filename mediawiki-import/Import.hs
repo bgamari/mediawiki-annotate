795515a243e5b2c9ea584681dd2389658770b9c9
Ben Gamari <ben@smart-cactus.org>
2017-08-29 22:27:02 -0400

    Import: Move some common utilities to Utils

--- mediawiki-import/Import.hs
+++ mediawiki-import/Import.hs
@@ -36,6 +36,7 @@ import Data.MediaWiki.Markup as Markup
 import CAR.Types
 import CAR.Utils
 import Entities
+import Utils
 
 newtype EncodedCbor a = EncodedCbor {getEncodedCbor :: BSL.ByteString}
 
@@ -170,49 +171,6 @@ docsToSkeletons siteId thisPage =
 parseSkeleton :: String -> Either String [PageSkeleton]
 parseSkeleton =
     fmap (docsToSkeletons (SiteId "Test Site") (PageId "Test Page")) . Markup.parse
-
-isTemplate :: Doc -> Bool
-isTemplate (Template{}) = True
-isTemplate _            = False
-
-isComment :: Doc -> Bool
-isComment (Comment{}) = True
-isComment _           = False
-
-takeXml :: T.Text -> [Doc] -> [Doc]
-takeXml tag (XmlTag tag' _ children : xs)
-  | T.toCaseFold tag == T.toCaseFold tag'
-  = children ++ takeXml tag xs
-takeXml tag (doc : xs)
-  | isXmlOpenClose tag doc
-  = takeXml tag xs
-takeXml tag (x:xs) = x : takeXml tag xs
-takeXml _   [] = []
-
-dropXml :: T.Text -> [Doc] -> [Doc]
-dropXml = replaceXml []
-
-replaceXml :: [Doc] -> T.Text -> [Doc] -> [Doc]
-replaceXml sub tag (doc : xs)
-  | isXml tag doc          = sub ++ replaceXml sub tag xs
-  | isXmlOpenClose tag doc = sub ++ replaceXml sub tag xs
-replaceXml sub tag (x:xs) = x : replaceXml sub tag xs
-replaceXml _   _   [] = []
-
-isXmlOpenClose, isXml :: T.Text -> Doc -> Bool
-isXmlOpenClose tag (XmlOpenClose tag' _)   =
-    T.toCaseFold tag == T.toCaseFold tag'
-isXmlOpenClose _   _                     = False
-
-isXml tag (XmlTag tag' _ _) =
-    T.toCaseFold tag == T.toCaseFold tag'
-isXml _   _              = False
-
-normPageName :: PageName -> PageName
-normPageName (PageName target) =
-    PageName $ normFirst target
-  where
-    normFirst t = (\(a,b) -> T.toUpper a `T.append` b) $ T.splitAt 1 t
 
 type TemplateTag = Text
 
@@ -398,21 +356,6 @@ toParaBody siteId thisPage = go
     go (ExternalLink _url (Just anchor))
                        = Just [ParaText $ T.pack anchor]
     go _               = Nothing
-
-getText :: Doc -> Maybe String
-getText (Text x)        = Just x
-getText (Char c)        = Just [c]
-getText  Bold           = Just ""
-getText  Italic         = Just ""
-getText  BoldItalic     = Just ""
-getText (InternalLink target [])  = Just $ T.unpack $ getPageName $ linkTargetPage target
-getText (InternalLink _ (xs:_))   = Just $ getAllText xs
-getText (ExternalLink _ (Just s)) = Just s
-getText _               = Nothing
-
-getAllText :: [Doc] -> String
-getAllText = concat . mapMaybe getText
-
 
 getPrefix :: (a -> Maybe b) -> [a] -> ([b], [a])
 getPrefix f = go []
---
+++ mediawiki-import/Utils.hs
@@ -0,0 +1,63 @@
+module Utils where
+
+import Data.Maybe
+import qualified Data.Text as T
+
+import Data.MediaWiki.Markup
+
+normPageName :: PageName -> PageName
+normPageName (PageName target) =
+    PageName $ normFirst target
+  where
+    normFirst t = (\(a,b) -> T.toUpper a `T.append` b) $ T.splitAt 1 t
+
+isTemplate :: Doc -> Bool
+isTemplate (Template{}) = True
+isTemplate _            = False
+
+isComment :: Doc -> Bool
+isComment (Comment{}) = True
+isComment _           = False
+
+takeXml :: T.Text -> [Doc] -> [Doc]
+takeXml tag (XmlTag tag' _ children : xs)
+  | T.toCaseFold tag == T.toCaseFold tag'
+  = children ++ takeXml tag xs
+takeXml tag (doc : xs)
+  | isXmlOpenClose tag doc
+  = takeXml tag xs
+takeXml tag (x:xs) = x : takeXml tag xs
+takeXml _   [] = []
+
+dropXml :: T.Text -> [Doc] -> [Doc]
+dropXml = replaceXml []
+
+replaceXml :: [Doc] -> T.Text -> [Doc] -> [Doc]
+replaceXml sub tag (doc : xs)
+  | isXml tag doc          = sub ++ replaceXml sub tag xs
+  | isXmlOpenClose tag doc = sub ++ replaceXml sub tag xs
+replaceXml sub tag (x:xs) = x : replaceXml sub tag xs
+replaceXml _   _   [] = []
+
+isXmlOpenClose, isXml :: T.Text -> Doc -> Bool
+isXmlOpenClose tag (XmlOpenClose tag' _)   =
+    T.toCaseFold tag == T.toCaseFold tag'
+isXmlOpenClose _   _                     = False
+
+isXml tag (XmlTag tag' _ _) =
+    T.toCaseFold tag == T.toCaseFold tag'
+isXml _   _              = False
+
+getText :: Doc -> Maybe String
+getText (Text x)        = Just x
+getText (Char c)        = Just [c]
+getText  Bold           = Just ""
+getText  Italic         = Just ""
+getText  BoldItalic     = Just ""
+getText (InternalLink target [])  = Just $ T.unpack $ getPageName $ linkTargetPage target
+getText (InternalLink _ (xs:_))   = Just $ getAllText xs
+getText (ExternalLink _ (Just s)) = Just s
+getText _               = Nothing
+
+getAllText :: [Doc] -> String
+getAllText = concat . mapMaybe getText
