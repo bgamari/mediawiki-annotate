f8dfd59c13baa0073d014b636745659ee66eab9f
Ben Gamari <ben@smart-cactus.org>
2017-10-02 18:59:21 -0400

    Agreement: Move agreement measures to simplir

--- assessment-eval/Agreement.hs
+++ assessment-eval/Agreement.hs
@@ -11,9 +11,9 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
 import Options.Applicative
 import System.FilePath
+
 import qualified SimplIR.Format.QRel as QRel
-
-
+import SimplIR.Assessment.Agreement
 import AssessmentEval
 
 opts :: Parser [FilePath]
@@ -39,83 +39,3 @@ main = do
                        ]
     putStrLn $ "Fleiss: "<>show (fleissKappa $ HM.elems assessments)
     return ()
-
-cohenKappa :: forall subj cat. (Eq cat, Hashable cat, Eq subj, Hashable subj)
-           => HM.HashMap subj cat
-           -> HM.HashMap subj cat
-           -> Double
-cohenKappa a b =
-    1 - (1 - po) / (1 - pe)
-  where
-    !agreementCount = length [ ()
-                             | (ka,kb) <- HM.elems inter
-                             , ka == kb
-                             ]
-    !po = realToFrac agreementCount / realToFrac allCount
-    !pe = realToFrac (sum $ HM.intersectionWith (*) na nb) / realToFrac allCount^(2::Int)
-    -- how many times a and b predict class k
-    na, nb :: HM.HashMap cat Int
-    !na = HM.fromListWith (+) [ (k, 1) | (_, (k, _)) <- HM.toList inter ]
-    !nb = HM.fromListWith (+) [ (k, 1) | (_, (_, k)) <- HM.toList inter ]
-
-    inter :: HM.HashMap subj (cat,cat)
-    !inter = HM.intersectionWith (,) a b
-    !allCount = HM.size inter
-
-fleissKappa :: forall subj cat. (Eq cat, Hashable cat, Eq subj, Hashable subj)
-            => [HM.HashMap subj cat]  -- ^ the assessments of each assessor
-            -> Double
-fleissKappa assessments' =
-    (barP - barPe) / (1 - barPe)
-  where
-    assessments = onlyOverlappingAssessments assessments'
-
-    -- n_i
-    numAssessments :: HM.HashMap subj Int
-    numAssessments = HM.fromListWith (+) [ (x, 1)
-                                         | a <- assessments
-                                         , x <- HM.keys a ]
-    -- N
-    numSubjects = HM.size numAssessments
-    totalAssessments = sum numAssessments
-
-    -- n_ij
-    nij :: HM.HashMap subj (HM.HashMap cat Int)
-    nij = HM.fromListWith (HM.unionWith (+))
-        [ (x, HM.singleton k 1)
-        | a <- assessments
-        , (x, k) <- HM.toList a
-        ]
-
-    -- p_j, probability that class k is predicted
-    pj :: HM.HashMap cat Double
-    pj = HM.fromListWith (+)
-         [ (k, realToFrac n / realToFrac totalAssessments)
-         | xs <- HM.elems nij
-         , (k, n) <- HM.toList xs
-         ]
-
-    barP :: Double
-    barP = (/ realToFrac numSubjects) $ sum
-           [ (inner / realToFrac ni / realToFrac (ni - 1)) - 1 / realToFrac (ni - 1)
-           | (x, njs) <- HM.toList nij
-           , let Just ni = x `HM.lookup` numAssessments
-           , let inner = sum [ (realToFrac v)^(2::Int)
-                             | v <- HM.elems njs
-                             ]
-           ]
-
-    barPe = sum [ v^(2 :: Int) | v <- HM.elems pj ]
-
-onlyOverlappingAssessments
-    :: forall subj cat. (Eq cat, Hashable cat, Eq subj, Hashable subj)
-    => [HM.HashMap subj cat] -> [HM.HashMap subj cat]
-onlyOverlappingAssessments assessments =
-    map (HM.filterWithKey overlaps) assessments
-  where
-    numAssessments :: HM.HashMap subj Int
-    numAssessments = HM.fromListWith (+) [ (x, 1)
-                                         | a <- assessments
-                                         , x <- HM.keys a ]
-    overlaps x _ = n > 1
-      where Just n = x `HM.lookup` numAssessments
