91a3c930556fdf98412417858aaf5b14b08cddda
Laura Dietz <dietz@cs.umass.edu>
2017-12-30 12:41:43 -0500

    fixing submodules

--- graph-expansion/BuildKbIndex.hs
+++ graph-expansion/BuildKbIndex.hs
@@ -134,7 +134,7 @@ writeRecords path recs = withFile path WriteMode $ \hdl ->
 pageIdToRecordId :: PageId -> Warc.RecordId
 pageIdToRecordId = RecordId . Uri . Utf8.toByteString . unPageId
 
--- d
+-- ds
 edgeDocModes :: Parser (IO ())
 edgeDocModes = subparser
     $ command "index" (info (helper <*> buildMode) mempty)
