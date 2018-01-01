007ac216a21520e00645c0ef29ae717f8c534a91
Laura Dietz <dietz@cs.umass.edu>
2018-01-01 18:04:21 -0500

    MapParagraphIds: deduplicate run files

--- filter-duplicates/MapParagraphIds.hs
+++ filter-duplicates/MapParagraphIds.hs
@@ -13,6 +13,8 @@ import CAR.FilterDuplicates.ConnectedComponent
 import CAR.FilterDuplicates.Utils
 import CAR.RunFile
 import CAR.QRelFile
+import CAR.Utils (nubWithKey)
+import Data.List
 
 
 
@@ -21,9 +23,10 @@ main = do
     mode <- execParser $ info (helper <*> modes) (progDesc "Converting paragraph ids from run files and qrels files according to duplicates mapping.\n\n")
     mode
 
-modes = subparser $ command "run" ( info (helper <*> runMode) mempty)
+modes = subparser $ command "run" ( info (helper <*> runMode) (progDesc "Translate run files according to duplication table"))
 --                     <> command "entitypassage-run" $ info (run <$> argument str (help "run file with entity passage entries"))
-                   <> command "qrel"  ( info (helper <*> qrelMode) mempty)
+                   <> command "qrel"  ( info (helper <*> qrelMode) (progDesc "Translate qrels according to duplication table") )
+                   <> command "nubrun"  ( info (helper <*> nubRunMode) (progDesc "Remove duplicate document names"))
 
 duplicatesArg :: Parser FilePath
 duplicatesArg = option str (short 'd' <> long "duplicates" <> help "duplicates file")
@@ -47,6 +50,7 @@ runMode =
                 where mapDocument entry = entry { carDocument = paragraphMapper (carDocument entry) }
           writeParagraphRun outputFile entries'
 
+
 qrelMode :: Parser (IO ())
 qrelMode =
     run <$> argument str (help "qrel file over paragraph ids")
@@ -65,8 +69,22 @@ qrelMode =
           writeParagraphQRel outputFile entries'
 
 
+nubRunMode :: Parser (IO ())
+nubRunMode =
+    run <$> argument str (help "run file over paragraph ids")
+        <*> outputArg
+    where
+      sameQueryId entry1 entry2 =
+          carQueryId entry1 == carQueryId entry2
 
-
+      run :: FilePath -> FilePath -> IO ()
+      run runFile outputFile = do
+          entries <- readParagraphRun runFile
+          let entries' = concat
+                       [ nubWithKey carDocument ranking
+                       | ranking <- groupBy sameQueryId entries
+                       ]
+          writeParagraphRun outputFile entries'
 
 
 mapToNewParagraphIds :: HM.HashMap ParagraphId ParagraphId -> ParagraphId -> ParagraphId
