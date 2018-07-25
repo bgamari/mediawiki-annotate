ad9aa5d19d6403452b4eac592622399b44e59b15
Laura Dietz <dietz@cs.umass.edu>
2018-07-24 22:36:47 -0400

    Cat.hs: allow to overwrite release name

--- trec-car-tools/Cat.hs
+++ trec-car-tools/Cat.hs
@@ -4,28 +4,32 @@ import Data.Semigroup hiding (option)
 import qualified Data.HashSet as HS
 import Options.Applicative
 import Codec.Serialise
+import qualified Data.Text as T
+import Data.Maybe
 
 import CAR.Types
 import CAR.ToolVersion
 
-args :: Parser (FilePath, [FilePath], Bool)
+args :: Parser (FilePath, [FilePath], Bool, Maybe T.Text)
 args =
-    (,,)
+    (,,,)
     <$> option str (short 'o' <> long "output" <> help "output file")
     <*> some (argument str (help "pages file" <> metavar "PAGES"))
     <*> switch (short 'O' <> long "outlines" <> help "merge outlines files")
-
+    <*> optional (option str (long "data-release-name" <> metavar "RELEASE" <> help "overwrite data release name"))
 main :: IO ()
 main = do
-    (output, files, outlines) <- execParser' 2 (helper <*> args) mempty
+    (output, files, outlines, newDataReleaseName) <- execParser' 2 (helper <*> args) mempty
     if outlines
-      then catFiles readOutlinesFileWithProvenance output files
-      else catFiles readPagesFileWithProvenance output files
+      then catFiles readOutlinesFileWithProvenance output files newDataReleaseName
+      else catFiles readPagesFileWithProvenance output files newDataReleaseName
 
 catFiles :: (File a, Serialise a)
-         => (FilePath -> IO (Provenance, [a])) -> FilePath -> [FilePath] -> IO ()
-catFiles read output files = do
+         => (FilePath -> IO (Provenance, [a])) -> FilePath -> [FilePath] -> Maybe T.Text -> IO ()
+catFiles read output files newDataReleaseName = do
     pages <- mapM read files
     let siteProvs = HS.toList $ foldMap (HS.fromList . siteProvenances . fst) pages
-        prov = (fst $ head pages) { siteProvenances = siteProvs }
-    writeCarFile output prov (foldMap snd pages)
+        prov = fst $ head pages
+        prov' = prov { siteProvenances = siteProvs
+                     , dataReleaseName = fromMaybe (dataReleaseName prov) newDataReleaseName }
+    writeCarFile output prov' (foldMap snd pages)
