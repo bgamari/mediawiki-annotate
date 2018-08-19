3aa3aef819770344c5dcc3781736352e46b8713c
Laura Dietz <dietz@cs.umass.edu>
2018-08-19 16:13:27 -0400

    TestEntityPassageParsing: adding test code

---
+++ trec-car-tools/TestEntityPassageParsing.hs
@@ -0,0 +1,43 @@
+import Data.Proxy
+import Control.Monad
+
+import qualified Data.HashSet as HS
+import Options.Applicative
+import Codec.Serialise
+
+import Control.Exception
+import Data.Ord
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Text as T
+import qualified Text.Regex as TR
+import qualified Data.Map.Strict as M
+import qualified Data.Sequence as Seq
+import qualified SimplIR.Format.TrecRunFile as Run
+import qualified Data.SmallUtf8 as Utf8
+
+
+import Debug.Trace
+
+import CAR.Types
+import CAR.ToolVersion
+
+args :: Parser String
+args = argument str (metavar "STR")
+main :: IO ()
+main = do
+    input <- execParser' 2 (helper <*> args) mempty
+    print $ parsing $ T.pack input
+
+parsing :: T.Text -> (T.Text, T.Text)
+parsing docName = (T.pack a, b)
+      where (a,b) =  case TR.matchRegex (TR.mkRegex "^([0-9a-f]+/)") (T.unpack docName) of
+                        Nothing -> trace "nothing" $ ("", docName)
+                        Just [] -> trace ("no matches in " <> (show docName)) $ ("", docName)
+                        Just [ paraSlash ] ->
+                                        let para = case T.unsnoc $ T.pack paraSlash of
+                                                    Just (para, _) -> trace ("justpara " <> show para) $ para
+                                                    Nothing -> T.pack $ trace ("parsePassageEntity: Issues with dropping slash of paraSlash " <> (show paraSlash)) $ ""
+                                            entity = T.pack $ drop (length paraSlash) (T.unpack docName)
+                                        in (T.unpack para, entity)
+                        xx -> trace ("parsePassageEntity: Multiple para matches "<> (show docName) <> " : "++ (show xx)) $ ("", T.pack "")
