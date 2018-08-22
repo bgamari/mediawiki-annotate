3f2efb48f904c978bd3af00268f319adf5cdd942
Ben Gamari <ben@smart-cactus.org>
2018-08-21 22:33:51 -0400

    develop.nix: Update

--- develop.nix
+++ develop.nix
@@ -1,10 +1,13 @@
 # For use with nix-shell
-{ nixpkgs ? (import <nixpkgs> {}) }:
+{ nixpkgs ? (import ./simplir/nixpkgs.nix {}) }:
 
 with nixpkgs;
 let
   trec-eval = callPackage ./simplir/trec-eval.nix {};
 in stdenv.mkDerivation {
-  name = "hi";
-  buildInputs = [ zlib icu icu.out expat lzma trec-eval kyotocabinet ];
+  name = "mediawiki-annotate-develop";
+  buildInputs = [
+    haskell.compiler.ghc843 binutils-unwrapped
+    zlib icu icu.out expat lzma trec-eval kyotocabinet
+  ];
 }
