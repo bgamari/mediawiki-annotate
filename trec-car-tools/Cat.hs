917aed956cf4285280ad4c7b87a735d81a929aec
Laura Dietz <dietz@cs.umass.edu>
2018-08-22 17:21:12 -0400

    Cat: implement cat for paragraphs

--- trec-car-tools/Cat.hs
+++ trec-car-tools/Cat.hs
@@ -10,19 +10,27 @@ import Data.Maybe
 import CAR.Types
 import CAR.ToolVersion
 
-args :: Parser (FilePath, [FilePath], Bool, Maybe T.Text)
+data MergeMode = MergePages | MergeParagraphs | MergeOutlines
+
+args :: Parser (FilePath, [FilePath], MergeMode, Maybe T.Text)
 args =
     (,,,)
     <$> option str (short 'o' <> long "output" <> help "output file")
-    <*> some (argument str (help "pages file" <> metavar "PAGES"))
-    <*> switch (short 'O' <> long "outlines" <> help "merge outlines files")
+    <*> some (argument str (help "pages file" <> metavar "CBOR-Files"))
+    <*> mergeMode
     <*> optional (option str (long "data-release-name" <> metavar "RELEASE" <> help "overwrite data release name"))
+  where mergeMode = flag' MergeParagraphs (short 'P' <> long "paragraphs" <> help "merge paragraphs files")
+                  <|> flag' MergeOutlines (short 'O' <> long "outlines" <> help "merge outlines files")
+                  <|> pure MergePages
+
+
 main :: IO ()
 main = do
-    (output, files, outlines, newDataReleaseName) <- execParser' 2 (helper <*> args) mempty
-    if outlines
-      then catFiles readOutlinesFileWithProvenance output files newDataReleaseName
-      else catFiles readPagesFileWithProvenance output files newDataReleaseName
+    (output, files, mergeMode, newDataReleaseName) <- execParser' 2 (helper <*> args) mempty
+    case mergeMode of
+      MergePages -> catFiles readPagesFileWithProvenance output files newDataReleaseName
+      MergeParagraphs -> catFiles readParagraphsFileWithProvenance output files newDataReleaseName
+      MergeOutlines -> catFiles readOutlinesFileWithProvenance output files newDataReleaseName
 
 catFiles :: (File a, Serialise a)
          => (FilePath -> IO (Provenance, [a])) -> FilePath -> [FilePath] -> Maybe T.Text -> IO ()
