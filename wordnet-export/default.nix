5e8a5ede286f5d1b29a71799df5a1fbc8a29facc
Ben Gamari <ben@smart-cactus.org>
2018-10-11 20:51:55 -0400

    nix: Add sha256s

--- wordnet-export/default.nix
+++ wordnet-export/default.nix
@@ -5,8 +5,8 @@ let
     let src = fetchFromGitHub {
         owner = "bgamari";
         repo = "ukb-hs";
-        rev = "fc789861c0e0340a6d91efa5871fee29d4909788";
-        sha256 = null;
+        rev = "4605b1d1b8acd2232b58bbe9fd970a0437432795";
+        sha256 = "0c3qnz7s80zf80v5k0x14yd2qmyp8s2919ds560jm1jcwzf9nzsx";
       };
     in import src { inherit haskellPackages; };
 
@@ -15,7 +15,7 @@ let
         owner = "bgamari";
         repo = "wordnet-parse";
         rev = "96f90c1e2d9924f3c65fc454bb9237ca38b83dd0";
-        sha256 = null;
+        sha256 = "0877219fwdccn75xizfgc112lf4ygx5xflp1drvwrlxrsiyr7hn7";
       };
     in haskellPackages.callCabal2nix "wordnet-parse" src {};
 in
