cad5ed984e5302ffa96ef7fb2f599dea94565817
Ben Gamari <ben@smart-cactus.org>
2020-05-19 19:15:14 -0400

    develop.nix

--- develop.nix
+++ develop.nix
@@ -1,12 +1,13 @@
 # For use with nix-shell
-{ nixpkgs ? (import ./ENT-rank/trec-car-tools-haskell/simplir/nixpkgs.nix {}) }:
+{ nixpkgs ? (import ./trec-car-tools-haskell/simplir/nixpkgs.nix {}) }:
 
 with nixpkgs;
 let
-  trec-eval = callPackage ./ENT-rank/trec-car-tools-haskell/simplir/trec-eval.nix {};
+  trec-eval = callPackage ./trec-car-tools-haskell/simplir/trec-eval.nix {};
 in stdenv.mkDerivation {
   name = "mediawiki-annotate-develop";
   buildInputs = [
+    haskell.compiler.ghc883
     curl binutils-unwrapped leveldb
     zlib icu icu.out expat lzma trec-eval kyotocabinet
     postgresql ghc cabal-install
