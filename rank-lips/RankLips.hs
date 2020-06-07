80074a00d8c91396a3ddea89720b00027aa7fa19
Laura Dietz <dietz@cs.umass.edu>
2020-06-06 23:25:44 -0400

    Rank-Lips: mini-batchable learning to rank

---
+++ rank-lips/RankLips.hs
@@ -0,0 +1,348 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Main where
+
+import Control.Concurrent.Async
+import Control.DeepSeq hiding (rwhnf)
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad
+import Control.Parallel.Strategies
+import Control.Lens (each)
+import Data.Coerce
+import Data.Tuple
+import Data.Semigroup hiding (All, Any, option)
+import Options.Applicative
+import System.IO
+import Data.Aeson
+import System.Random
+import GHC.Generics
+import GHC.Stack
+import Control.Exception
+import System.FilePath
+
+import qualified Data.Set as S
+import qualified Data.Map.Strict as M
+import qualified Data.Map.Lazy as ML
+import qualified Data.HashMap.Strict as HM
+import qualified Data.HashSet as HS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Generic as VG
+import qualified Text.PrettyPrint.Leijen.Text as PP
+import Data.List
+import Data.Maybe
+import Data.Foldable as Foldable
+import Data.Hashable
+import Control.Concurrent
+import Control.Concurrent.Map
+import Data.List.Split
+import qualified Codec.Serialise as CBOR
+import qualified SimplIR.Format.TrecRunFile as SimplirRun
+
+
+
+-- import CAR.Retrieve as Retrieve
+-- import qualified CAR.RunFile as CarRun
+-- import CAR.Utils
+-- import GridFeatures
+
+import qualified SimplIR.SimpleIndex as Index
+import SimplIR.LearningToRank
+import SimplIR.LearningToRankWrapper
+import qualified SimplIR.FeatureSpace as F
+import SimplIR.FeatureSpace (FeatureSpace, FeatureVec)
+import SimplIR.FeatureSpace.Normalise
+import SimplIR.Intern
+
+-- import qualified CAR.RunFile as CAR.RunFile
+-- todo switch to import qualified SimplIR.Format.TrecRunFile as Simplir.RunFile
+import qualified SimplIR.Format.QRel as QRel
+import qualified SimplIR.Ranking as Ranking
+-- import MultiTrecRunFile
+
+import Control.Monad
+
+-- import GridFeatures
+import TrainAndSave
+
+import Debug.Trace  as Debug
+import CAR.Types (ParagraphId)
+
+type NumResults = Int
+
+
+newtype Feat = Feat { featureName :: T.Text }
+    deriving (Eq, Show, Ord, Read)
+
+data FeatureVariant = FeatScore   
+    deriving (Eq, Show, Ord,  Read, Enum, Bounded)  -- Todo: Hashable
+-- | FeatRecipRank
+
+type RankEntry = SimplirRun.DocumentName
+
+
+-- defaultMiniBatchParams :: MiniBatchParams
+-- defaultMiniBatchParams = MiniBatchParams 1 100 0
+
+
+-- relevance :: M.Map (queryId, docId) IsRelevant
+-- relevance = M.fromList [ ((qid, doc), rel)
+--                             | QRel.Entry qid doc rel <- qrel
+--                             ]
+
+-- qrel :: [QRel.Entry SimplirRun.QueryId doc IsRelevant]
+-- qrel = undefined
+
+includeCv = True
+
+-- startDir = "/home/dietz/trec-car/code/mediawiki-annotate/data/rank-lips"
+startDir = "/home/dietz/trec-car/code/mediawiki-annotate/data/eal-single"
+-- startDir = "/home/dietz/trec-car/code/mediawiki-annotate/data/eal-tools"
+
+dirPrefix = startDir </> "features"
+outputFilePrefix :: FilePath
+outputFilePrefix =  ""
+
+miniBatchParams = defaultMiniBatchParams
+
+-- type TrainData f s = M.Map Q [(DocId, FeatureVec f s Double, Rel)]
+
+main :: IO ()
+main = do
+
+    let modelFile =  startDir </> "model"
+        qrelFile =  startDir </> "demo-train.qrel"
+
+    let updatedModelFile = modelFile <> ".json"
+        (MiniBatchParams batchSteps batchSize evalSteps) = miniBatchParams
+        evalSteps' = evalSteps +2
+--               pageRankSteps = evalSteps'
+    -- putStrLn "loading model"
+    -- Just (SomeModel model) <-  Data.Aeson.decode @(SomeModel Feat) <$> BSL.readFile modelFile
+    -- putStrLn "loaded model."
+
+
+    qrelData <- QRel.readQRel qrelFile
+                :: IO [QRel.Entry SimplirRun.QueryId SimplirRun.DocumentName IsRelevant]
+
+    let qrelMap :: M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName IsRelevant)
+        qrelMap = M.fromListWith (<>)
+                [ (queryId, M.singleton documentName relevance)
+                | QRel.Entry {..}  <- qrelData 
+                ]
+
+        lookupQrel :: IsRelevant -> SimplirRun.QueryId -> SimplirRun.DocumentName -> IsRelevant
+        lookupQrel defaultRel queryId docName =
+            case queryId `M.lookup` qrelMap of
+                Nothing -> defaultRel
+                Just dMap -> case  docName `M.lookup` dMap of
+                                Nothing -> defaultRel
+                                Just r -> r
+
+
+
+
+
+ 
+    let runFilenames :: [FilePath]
+        runFilenames = ["PARA_CONTENT_TO_ASPECT_HEADER_TFIDF", "SENT_CONTENT_TO_ASPECT_HEADER_BM25", "SIMP_SENT_ENTITIES_OVERLAP"]
+        -- runFilenames = ["f1","f2","f3"]
+
+        featureNames :: S.Set Feat
+        featureNames = S.fromList $ [ augmentFname ft run 
+                                    | run <-  runFilenames
+                                    , ft <- [minBound @FeatureVariant .. maxBound @FeatureVariant]
+                                    ]
+
+    F.SomeFeatureSpace (fspace:: F.FeatureSpace Feat ph) <- pure $ F.mkFeatureSpace featureNames
+
+    runFiles <- loadRunFiles  dirPrefix runFilenames
+    putStrLn $ " loadRunFiles " <> (unwords $ fmap fst runFiles)
+
+    let featureDataMap = runFilesToFeatureVectorsMap fspace runFiles
+        featureDataList = fmap M.toList featureDataMap
+
+
+    -- let docFeatures :: M.Map (QueryId, DocId) (FeatureVec Feat ph Double)
+    --     docFeatures = M.fromList
+    --                 $ [ ((q,doc), F.fromList fspace featureList )
+    --                   | (q, entries) <- M.toList collapsedRun
+    --                   , (MultiRankingEntry {multiRankingEntryAll = list}) <- entries
+    --                   , let featureList = 
+    --                          [ (fname, fscore)
+    --                          | (fname, CarRun.RankingEntry{carScore=fscore}) <- list
+    --                          ] 
+    --                   ]
+
+        allDataMap :: M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName  (FeatureVec Feat ph Double, Rel))
+        allDataMap = augmentWithQrelsMap_ (lookupQrel NotRelevant) featureDataMap
+
+        allDataList :: M.Map SimplirRun.QueryId [( SimplirRun.DocumentName, FeatureVec Feat ph Double, Rel)]
+        allDataList = augmentWithQrelsList_ (lookupQrel NotRelevant) featureDataList
+
+
+
+    -- let augmentWithQrels :: QueryId -> Ranking Double RankEntry -> Ranking Double (RankEntry, IsRelevant)
+    --     augmentWithQrels query =
+    --         fmap (\page -> (page, lookupQrel NotRelevant query page))
+ 
+        totalRels :: M.Map SimplirRun.QueryId Int
+        !totalRels = fmap countRel qrelMap
+                    where countRel :: M.Map x IsRelevant -> Int
+                          countRel m = length [ r
+                                                | (_, r) <- M.toList m
+                                                , QRel.isPositive r
+                                                ]
+
+        metric :: ScoringMetric IsRelevant SimplirRun.QueryId
+        metric = meanAvgPrec (\q -> fromMaybe 0 $ q `M.lookup` totalRels)  Relevant
+
+        -- !someKindOfTrainingData = M.fromList $ [(q,q) | q <- intersect (M.keys totalRels) (M.keys featureGraphs) ] -- totalRels does not include queries for which there is no training data
+
+    gen0 <- newStdGen
+
+    -- only get the edge features out
+    -- Just (F.FeatureMappingInto toEdgeVec) <- pure $ F.mapFeaturesInto (modelFeatures model) (edgeFSpace fspaces) (either (const Nothing) Just)
+    
+
+
+    train includeCv fspace allDataList qrelData miniBatchParams outputFilePrefix modelFile
+
+
+-- type TrainData ph = M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName  (FeatureVec Feat ph Double, Rel))
+-- type TrainData f s = M.Map Q [(DocId, FeatureVec f s Double, Rel)]
+
+
+
+train :: Bool
+      -> F.FeatureSpace Feat s
+      -> TrainData Feat s
+      -> [QRel.Entry SimplirRun.QueryId doc IsRelevant]
+      -> MiniBatchParams
+      -> FilePath
+      -> FilePath
+      -> IO()
+train includeCv fspace allData qrel miniBatchParams outputFilePrefix modelFile =  do
+              let metric :: ScoringMetric IsRelevant SimplirRun.QueryId
+                  !metric = meanAvgPrec (totalRelevantFromQRels qrel) Relevant
+                  totalElems = getSum . foldMap ( Sum . length ) $ allData
+                  totalPos = getSum . foldMap ( Sum . length . filter (\(_,_,rel) -> rel == Relevant)) $ allData
+
+              putStrLn $ "Feature dimension: "++show (F.dimension $ F.featureSpace $ (\(_,a,_) -> a) $ head' $ snd $ M.elemAt 0 allData)
+              putStrLn $ "Training model with (trainData) "++ show (M.size allData) ++
+                        " queries and "++ show totalElems ++" items total of which "++
+                        show totalPos ++" are positive."
+              let
+                  displayTrainData :: Show f => TrainData f s -> [String]
+                  displayTrainData trainData =
+                    [ show k ++ " " ++ show d ++ " " ++ show r ++ " -> "++ prettyFv
+                    | (k,list) <- M.toList trainData
+                    , (d,fvec, r) <- list
+                    , let prettyFv = unlines $ fmap show $ F.toList fvec
+                    ]
+
+              putStrLn $ "Training Data = \n" ++ intercalate "\n" (take 10 $ displayTrainData $ force allData)
+              gen0 <- newStdGen  -- needed by learning to rank
+              trainMe includeCv miniBatchParams (EvalCutoffAt 100) gen0 allData fspace metric outputFilePrefix modelFile
+
+
+
+augmentFname :: FeatureVariant -> FilePath -> Feat
+augmentFname featureVariant fname = Feat $ T.pack $ fname <> "-" <> show featureVariant
+
+loadRunFiles :: FilePath -> [FilePath] ->  IO [(FilePath, [SimplirRun.RankingEntry])] 
+loadRunFiles prefix inputRuns = do
+    mapM (\fname -> (fname,) <$> SimplirRun.readRunFile (prefix </> fname)) inputRuns    
+                
+
+augmentWithQrelsMap_ :: (SimplirRun.QueryId -> SimplirRun.DocumentName -> Rel) 
+                 -> M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName (F.FeatureVec Feat ph Double)) 
+                 -> M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName (F.FeatureVec Feat ph Double, Rel))
+augmentWithQrelsMap_ lookupQrel qData =
+    M.mapWithKey mapQueries qData
+  where mapQueries :: SimplirRun.QueryId -> (M.Map SimplirRun.DocumentName d) -> M.Map SimplirRun.DocumentName (d, Rel)
+        mapQueries query dMap =
+            M.mapWithKey mapDocs dMap
+          where mapDocs :: SimplirRun.DocumentName -> d -> (d,Rel)
+                mapDocs doc feats =
+                    (feats, lookupQrel query doc)
+
+
+augmentWithQrelsList_ :: (SimplirRun.QueryId -> SimplirRun.DocumentName -> Rel) 
+                 -> M.Map SimplirRun.QueryId [( SimplirRun.DocumentName, (F.FeatureVec Feat ph Double))] 
+                 -> M.Map SimplirRun.QueryId [(SimplirRun.DocumentName, F.FeatureVec Feat ph Double, Rel)]
+augmentWithQrelsList_ lookupQrel qData =
+    M.mapWithKey mapQueries qData
+  where mapQueries :: SimplirRun.QueryId -> [(SimplirRun.DocumentName, d)] -> [(SimplirRun.DocumentName, d, Rel)]
+        mapQueries query dMap =
+            fmap mapDocs dMap
+          where mapDocs :: (SimplirRun.DocumentName, d) -> (SimplirRun.DocumentName, d,Rel)
+                mapDocs (doc, feats) =
+                    (doc, feats, lookupQrel query doc)
+
+
+runFilesToFeatureVectorsMap :: forall ph . F.FeatureSpace Feat ph 
+                          -> [(FilePath, [SimplirRun.RankingEntry])] 
+                          -> M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName (F.FeatureVec Feat ph Double))
+runFilesToFeatureVectorsMap fspace runData = 
+    let features:: M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName [(Feat, Double)]) -- FeatureVec Feat ph Double)]
+        features = M.fromListWith (M.unionWith (<>))
+                 $ [ (queryId, 
+                        M.fromListWith (<>)
+                        [(documentName 
+                         ,  [ ((augmentFname FeatScore fname), documentScore)
+                            -- , ((augmentFname FeatRecipRank fname), (1.0/(realToFrac documentRank)))  
+                            ]
+                        )]
+                      )
+                    | (fname, rankingEntries) <- runData
+                    , SimplirRun.RankingEntry {..} <- rankingEntries
+                    ]
+                    -- ToDo Where are default values handled?
+
+        featureVectors :: M.Map SimplirRun.QueryId (M.Map SimplirRun.DocumentName (F.FeatureVec Feat ph Double))
+        featureVectors =  fmap featureVectorize features           
+
+    in featureVectors
+  where featureVectorize :: M.Map SimplirRun.DocumentName [(Feat, Double)] -> M.Map SimplirRun.DocumentName (FeatureVec Feat ph Double)
+        featureVectorize docFeatureList =
+            fmap (F.fromList fspace)  docFeatureList
+
+
+--   where makeFeatureVectors ::  [(Feat, Double)] -> FeatureVec Feat ph Double
+--         makeFeatureVectors featureList = F.fromList fspace featureList
+
+
+-- data RankingEntry = RankingEntry { queryId       :: !QueryId
+--                                  , documentName  :: !DocumentName
+--                                  , documentRank  :: !Rank
+--                                  , documentScore :: !Score
+--                                  , methodName    :: !MethodName
+--                                  }
+--                   deriving (Show)
+
+
+
+head' :: HasCallStack => [a] -> a
+head' (x:_) = x
+head' [] = error $ "head': empty list"
---
+++ rank-lips/src/TrainAndSave.hs
@@ -0,0 +1,292 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+
+module TrainAndSave where
+
+import Data.Aeson
+
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text as T
+import Data.List
+import Data.Foldable as Foldable
+import Data.Function
+import Data.Bifunctor
+import System.Random
+import Control.Parallel.Strategies
+import Control.Concurrent.Map
+import Control.DeepSeq
+
+import CAR.Types hiding (Entity)
+import qualified CAR.RunFile as CarRun
+
+import SimplIR.LearningToRank
+import SimplIR.LearningToRankWrapper
+import SimplIR.FeatureSpace (FeatureSpace, FeatureVec)
+
+-- import qualified CAR.RunFile as CAR.RunFile
+import qualified SimplIR.Format.QRel as QRel
+import qualified SimplIR.Ranking as Ranking
+import SimplIR.TrainUtils
+
+import Debug.Trace as Debug
+
+import qualified SimplIR.Format.TrecRunFile as SimplirRun
+
+
+type Q = SimplirRun.QueryId
+type DocId = SimplirRun.DocumentName
+type Rel = IsRelevant
+type TrainData f s = M.Map Q [(DocId, FeatureVec f s Double, Rel)]
+-- type ReturnWithModelDiagnostics a = (a, [(String, Model, Double)])
+type FoldRestartResults f s = Folds (M.Map Q [(DocId, FeatureVec f s Double, Rel)],
+                                    [(Model f s, Double)])
+type BestFoldResults f s = Folds (M.Map Q [(DocId, FeatureVec f s Double, Rel)], (Model f s, Double))
+
+
+trainMe :: forall f s. (Ord f, Show f)
+        => Bool
+        -> MiniBatchParams
+        -> EvalCutoff
+        -> StdGen
+        -> TrainData f s
+        -> FeatureSpace f s
+        -> ScoringMetric IsRelevant Q
+        -> FilePath
+        -> FilePath
+        -> IO ()
+trainMe includeCv miniBatchParams evalCutoff gen0 trainData fspace metric outputFilePrefix modelFile = do
+          -- train me!
+          let nRestarts = 5
+              nFolds = 5
+
+
+          -- folded CV
+                                -- todo load external folds
+              !folds = force $ mkSequentialFolds nFolds (M.keys trainData)
+          putStrLn "made folds"
+          putStrLn $ unwords [ show $ length f | f <- getFolds folds ]
+
+          let trainFun :: FoldIdx -> TrainData f s -> [(Model f s, Double)]
+              trainFun foldIdx =
+                  take nRestarts . trainWithRestarts miniBatchParams evalCutoff gen0 metric infoStr fspace
+                where
+                  infoStr = show foldIdx
+
+              foldRestartResults :: Folds (M.Map  Q [(DocId, FeatureVec f s Double, Rel)], [(Model f s, Double)])
+              foldRestartResults = kFolds trainFun trainData folds
+
+              strat :: Strategy (Folds (a, [(Model f s, Double)]))
+              strat = parTraversable (evalTuple2 r0 (parTraversable rdeepseq))
+          
+          putStrLn "full Train"
+          -- full train
+          let fullRestarts = withStrategy (parTraversable rdeepseq)
+                             $ take nRestarts $ trainWithRestarts miniBatchParams evalCutoff gen0 metric "full" fspace trainData
+              (model, trainScore) = Debug.trace ("full Train - best Model") 
+                                  $    bestModel $  fullRestarts
+              fullActions = Debug.trace ("full Train - dump Model")
+                          $ dumpFullModelsAndRankings trainData (model, trainScore) metric outputFilePrefix modelFile
+
+
+          putStrLn "CV Train"
+          if includeCv
+            then do
+              foldRestartResults' <- withStrategyIO strat foldRestartResults
+              let cvActions = dumpKFoldModelsAndRankings foldRestartResults' metric outputFilePrefix modelFile
+              putStrLn "concurrently: CV Train"
+              mapConcurrentlyL_ 24 id $ fullActions ++ cvActions
+            else
+              mapConcurrentlyL_ 24 id $ fullActions
+          putStrLn "dumped all models and rankings"
+
+trainWithRestarts
+    :: forall f s. (Show f)
+    => MiniBatchParams
+    -> EvalCutoff
+    -> StdGen
+    -> ScoringMetric IsRelevant Q
+    -> String
+    -> FeatureSpace f s
+    -> TrainData f s
+    -> [(Model f s, Double)]
+       -- ^ an infinite list of restarts
+trainWithRestarts miniBatchParams evalCutoff gen0 metric info fspace trainData =
+  let trainData' = discardUntrainable trainData
+
+      rngSeeds :: [StdGen]
+      rngSeeds = unfoldr (Just . System.Random.split) gen0
+
+      restartModel :: Int -> StdGen -> (Model f s, Double)
+      restartModel restart =
+          learnToRank miniBatchParams
+                      (defaultConvergence info' 1e-2 10 0)
+                      evalCutoff trainData' fspace metric
+        where
+          info' = info <> " restart " <> show restart
+      modelsWithTrainScore :: [(Model f s,Double)]
+      modelsWithTrainScore = zipWith restartModel [0..] rngSeeds
+     in modelsWithTrainScore
+
+
+discardUntrainable :: TrainData f s -> TrainData f s
+discardUntrainable evalData =
+    M.filter hasPosAndNeg  evalData
+  where
+    hasPosAndNeg list =
+        let hasPos = any (\(_,_,r) -> r == Relevant) list
+            hasNeg = any (\(_,_,r) -> r /= Relevant) list
+        in hasPos && hasNeg
+
+
+bestPerFold :: FoldRestartResults f s -> BestFoldResults f s
+bestPerFold = fmap (second bestModel)
+
+bestModel ::  [(Model f s, Double)] -> (Model f s, Double)
+bestModel = maximumBy (compare `on` snd)
+
+
+bestRankingPerFold :: forall f  s. ()
+                   => BestFoldResults f s
+                   -> Folds (M.Map Q (Ranking SimplIR.LearningToRank.Score (DocId, Rel)))
+bestRankingPerFold bestPerFold' =
+    fmap (\(testData, ~(model, _trainScore))  ->  rerankRankings' model testData) bestPerFold'
+
+
+
+dumpKFoldModelsAndRankings
+    :: forall f s. (Ord f, Show f)
+    => FoldRestartResults f s
+    -> ScoringMetric IsRelevant Q
+    -> FilePath
+    -> FilePath
+    -> [IO ()]
+dumpKFoldModelsAndRankings foldRestartResults metric outputFilePrefix modelFile =
+    let bestPerFold' :: Folds (M.Map Q [(DocId, FeatureVec f s Double, Rel)], (Model f s, Double))
+        bestPerFold' = bestPerFold foldRestartResults
+
+        bestRankingPerFold' :: Folds (M.Map Q (Ranking SimplIR.LearningToRank.Score (DocId, Rel)))
+        bestRankingPerFold' = bestRankingPerFold bestPerFold'
+
+        testRanking ::   M.Map Q (Ranking SimplIR.LearningToRank.Score (DocId, Rel))
+        testRanking = fold bestRankingPerFold'
+
+        _testScore = metric testRanking
+
+--         dumpAll = [ do storeRankingData outputFilePrefix ranking metric modelDesc
+--                        storeModelData outputFilePrefix modelFile model trainScore modelDesc
+--                   | (foldNo, ~(testData, restartModels))  <- zip [0..] $ toList foldRestartResults
+--                   , (restartNo, ~(model, trainScore)) <- zip [0..] restartModels
+--                   , let ranking = rerankRankings' model testData
+--                   , let modelDesc = "fold-"<> show foldNo <> "-restart-"<> show restartNo
+--                   ]
+
+        dumpAll = []
+
+        dumpBest =
+            [ do storeRankingData outputFilePrefix ranking metric modelDesc
+                 storeModelData outputFilePrefix modelFile model trainScore modelDesc
+            | (foldNo, (testData,  ~(model, trainScore)))  <- zip [0 :: Integer ..]
+                                                              $ toList bestPerFold'
+            , let ranking = rerankRankings' model testData
+            , let modelDesc = "fold-"<> show foldNo <> "-best"
+            ]
+
+        dumpKfoldTestRanking = storeRankingData outputFilePrefix testRanking metric modelDesc
+          where modelDesc = "test"
+
+    in dumpAll ++ dumpBest ++ [dumpKfoldTestRanking]
+
+
+
+dumpFullModelsAndRankings
+    :: forall f s. (Ord f, Show f)
+    => M.Map Q [(DocId, FeatureVec f s Double, Rel)]
+    -> (Model f s, Double)
+    -> ScoringMetric IsRelevant SimplirRun.QueryId
+    -> FilePath
+    -> FilePath
+    -> [IO()]
+dumpFullModelsAndRankings trainData (model, trainScore) metric outputFilePrefix modelFile =
+    let modelDesc = "train"
+        trainRanking = rerankRankings' model trainData
+    in [ storeRankingData outputFilePrefix trainRanking metric modelDesc
+       , storeModelData outputFilePrefix modelFile model trainScore modelDesc
+       ]
+
+
+
+
+l2rRankingToRankEntries :: SimplirRun.MethodName
+                        -> M.Map Q (Ranking SimplIR.LearningToRank.Score (SimplirRun.DocumentName, Rel))
+                        -> [SimplirRun.RankingEntry]
+l2rRankingToRankEntries methodName rankings =
+  [ SimplirRun.RankingEntry { queryId = query
+                             , documentName = doc
+                             , documentRank = rank
+                             , documentScore = rankScore
+                             , methodName = methodName
+                             }
+  | (query, ranking) <- M.toList rankings
+  , ((rankScore, (doc, _rel)), rank) <- Ranking.toSortedList ranking `zip` [1..]
+  ]
+
+
+-- Train model on all data
+storeModelData :: (Show f, Ord f)
+               => FilePath
+               -> FilePath
+               -> Model f s
+               -> Double
+               -> [Char]
+               -> IO ()
+storeModelData outputFilePrefix modelFile model trainScore modelDesc = do
+  putStrLn $ "Model "++modelDesc++ " train metric "++ (show trainScore) ++ " MAP."
+  let modelFile' = outputFilePrefix++modelFile++"-model-"++modelDesc++".json"
+  BSL.writeFile modelFile' $ Data.Aeson.encode model
+  putStrLn $ "Written model "++modelDesc++ " to file "++ (show modelFile') ++ " ."
+
+
+
+storeRankingData ::  FilePath
+               -> M.Map  Q (Ranking Double (QRel.DocumentName, IsRelevant))
+               -> ScoringMetric IsRelevant Q
+               -> String
+               -> IO ()
+storeRankingData outputFilePrefix ranking metric modelDesc = do
+  putStrLn $ "Model "++modelDesc++" test metric "++ show (metric ranking) ++ " MAP."
+  SimplirRun.writeRunFile (outputFilePrefix++"-model-"++modelDesc++".run")
+       $ l2rRankingToRankEntries (T.pack $ "l2r "++modelDesc)
+       $ ranking
+
+-- todo avoid duplicateion with storeRankingData
+storeRankingDataNoMetric :: FilePath
+                         -> M.Map SimplirRun.QueryId (Ranking Double (QRel.DocumentName, Rel))
+                         -> String
+                         -> IO ()
+storeRankingDataNoMetric outputFilePrefix ranking modelDesc = do
+  putStrLn $ "Model "++modelDesc++" .. no metric.."
+  SimplirRun.writeRunFile (outputFilePrefix++"-model-"++modelDesc++".run")
+       $ l2rRankingToRankEntries (T.pack $ "l2r "++modelDesc)
+       $ ranking
+
